2820c0f81247e5b2298cddcb7ed006391fb337ec
Tiago de Almeida Paz <tiagopaz.dev@gmail.com>
2021-01-11 18:11:11 -0300

    requisito 18 completo

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ const emailPhoneValue = document.querySelector('#user-email-phone');
 loginBtn.addEventListener('click', function () {
   alert(emailPhoneValue.value);
 });
+
+const registerBtn = document.querySelector('#facebook-register');
+const inputs = document.querySelectorAll('.input');
+
+registerBtn.addEventListener('click', function () {
+  for (let index = 0; index < inputs.length; index += 1) {
+    if (inputs[index].value === '') {
+      inputs[index].value = 'Campos inválidos';
+    }
+  }
+});
