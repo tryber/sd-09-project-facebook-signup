37373a6948328fec16aeed29f9879ac08dccb5ea
BernardoFontes <bfontes.1997@gmail.com>
2021-01-11 19:27:49 -0300

    requisito 18

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 const email = document.querySelector('#user-email-phone');
 const botao = document.querySelector('#button-login');
+const botaoCadastro = document.querySelector('#facebook-register');
+const newUser = document.querySelector('.usuario');
 botao.addEventListener('click', () => alert(email.value));
+botaoCadastro.addEventListener('click', (event) =>  {
+    const mensagemErro = document.createTextNode('Campos inválidos');
+    event.preventDefault();
+    // Baseado no código do Hugo Braga da turma 8
+    const resultado = newUser.checkValidity();
+    if (!resultado) {
+      novoUsuario.appendChild(mensagemErro);
+    }
+  });
