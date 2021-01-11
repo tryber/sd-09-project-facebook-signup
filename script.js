2c00318d8121d6e6a7a1712d7a5758425dd49d43
Marcelo Campos <marcelo.campos.economista@gmail.com>
2021-01-11 17:26:47 -0300

    Adiciona o requisito 18

--- script.js
+++ script.js
@@ -2,3 +2,19 @@ const getButton = document.querySelector('#button-login');
 getButton.addEventListener('click', function () {
   alert(document.querySelector('#user-email-phone').value);
 });
+
+function checkEmptyFields() {
+  const getInputs = document.querySelectorAll('#register-form input');
+  for (index = 0; index < getInputs.length; index += 1) {
+    if ( getInputs[index].value === '' ) {
+      alert('Campos inválidos')
+    }
+    continue;
+  }
+}
+
+const getRegisterBnt = document.querySelector('#facebook-register');
+getRegisterBnt.addEventListener('click', function (event) {
+  event.preventDefault();
+  checkEmptyFields();
+});
