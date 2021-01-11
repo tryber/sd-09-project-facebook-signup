c2ee0df47aa2dca90efeaf8409518595119099ff
Pedro Henrique <pedroenrique.400@gmail.com>
2021-01-11 18:17:16 -0300

    requisito 18/2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const buttonLogin = document.querySelector('#button-login');
 const userEmailPhone = document.querySelector('#user-email-phone');
-const buttonFacebookRegister = document.querySelector('.facebook-register');
+const buttonFacebookRegister = document.querySelector('#facebook-register');
 
 buttonFacebookRegister.addEventListener('click', () => {
 const completeForm = document.querySelectorAll('#register-form input');
