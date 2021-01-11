3a4030e99ce7f98b858ba95c4f3d318bbad2e869
Ricardo Sbeghen <ricardo.sbeghen@gmail.com>
2021-01-11 17:29:15 -0300

    Code climate

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function loginAlert() {
   const loginBtn = document.getElementById('button-login');
-  loginBtn.addEventListener('click', function(){
+  loginBtn.addEventListener('click', function() {
     alert(document.getElementById('user-email-phone').value);
   });
 }
