62aa8f670f8910a66ffe1ecdc49805ccb20d0d7a
Qu4k3r <neves0007@gmail.com>
2021-01-11 17:38:51 -0300

    Code climate

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-let btnEntrar = document.getElementById('button-login');
-let inputEmail = document.getElementById('user-email');
-let inputPw = document.getElementById('user-password');
+const btnEntrar = document.getElementById('button-login');
+const inputEmail = document.getElementById('user-email');
+const inputPw = document.getElementById('user-password');
 
 function addlistener(obj, func) {
   obj.addEventListener('click', func);
 }
 
 function alertMsgLogIn() {
-  alert('Email: ' + inputEmail.value + '\nSenha: ' + inputPw.value);
+  alert(`Email: ${inputEmail.value} | Senha: ${inputPw.value}`);
 }
 
 addlistener(btnEntrar, alertMsgLogIn);
