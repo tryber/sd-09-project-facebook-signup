0e7185e8380b1738105a60ab94b4b0fc348e0dc6
Luciano_Lodi <lrlodi@gmail.com>
2021-01-11 16:45:36 -0300

    Correcoes de CC

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const buttonLogin = document.querySelector('#button-login');
 const emailOrPhone = document.querySelector('#user-email-phone');
-buttonLogin.addEventListener('click', function() {
-    alert(emailOrPhone.value);
-})
+buttonLogin.addEventListener('click', function () {
+  alert(emailOrPhone.value);
+});
