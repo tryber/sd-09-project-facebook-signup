eeb4f8dd4755512244e3283c904d151e356353a3
Pedro Henrique <pedro.876.ph@gmail.com>
2021-01-11 17:05:21 -0300

    requisito 7 ajustes do alert

---
+++ script.js
@@ -0,0 +1,8 @@
+function windowAlert() {
+  const inputUser = document.querySelector('#user-email-phone');
+  const inputPassword = document.querySelector('#user-password');
+  alert(`Usuário: ${inputUser.value}, Senha: ${inputPassword.value}`);
+}
+
+const btnLogin = document.querySelector('#button-login')
+btnLogin.addEventListener('click', windowAlert);
