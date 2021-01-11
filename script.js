ef19a554856c6000fb9efd99dcb54e2d6e3b1c89
Pedro Henrique <pedroenrique.400@gmail.com>
2021-01-11 20:59:55 -0300

    Refaz o requisito da mensagem de Erro

--- script.js
+++ script.js
@@ -2,12 +2,17 @@ const buttonLogin = document.querySelector('#button-login');
 const userEmailPhone = document.querySelector('#user-email-phone');
 const buttonFacebookRegister = document.querySelector('#facebook-register');
 const radioCustom = document.querySelector('#custom');
+const registerForm = document.querySelector('#register-form');
 
 buttonFacebookRegister.addEventListener('click', () => {
   const completeForm = document.querySelectorAll('#register-form input');
   for (let index = 0; index < completeForm.length; index += 1) {
     if (completeForm[index].value === '') {
-      return alert('Campos inválidos');
+      let erroMensage = document.createElement('p');
+      erroMensage.innerText = 'Campos Inválidos';
+      erroMensage.style.color = 'red';
+      erroMensage.style.fontSize = '15px';
+      return registerForm.appendChild(erroMensage);
     }
   }
   return 0;
@@ -28,7 +33,6 @@ function createField() {
 }
 
 radioCustom.addEventListener('click', () => {
-  const registerForm = document.querySelector('#register-form');
   const registerSon = document.querySelector('#register-form .gender-custom');
 
   if (registerSon === null) {
