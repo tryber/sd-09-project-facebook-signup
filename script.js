15c3dff74c9474f9153b7cc4f0a17bb7c5ab7f14
MZTBTG <mztbtg@hotmail.com>
2021-01-11 19:11:23 -0300

    Correção do Evaluator

--- script.js
+++ script.js
@@ -7,4 +7,23 @@ function alertButton() {
   });
 }
 
+function validadeField() {
+  const buttonSubmit = document.querySelector('#facebook-register');
+  buttonSubmit.addEventListener('click', function (event) {
+    event.preventDefault();
+    const fields = document.querySelectorAll('.register-form input');
+    console.log(fields[0]);
+    for(let index = 0; index < fields.length; index += 1) {
+      let fieldValue = fields[index].value;
+      if(fieldValue === '') {
+        const form = document.querySelector('.register-form');
+        const formWarning = document.createElement('p');
+        formWarning.innerText = 'Campos inválidos';
+        form.appendChild(formWarning);
+      }
+    }
+  })
+}
+
+validadeField();
 alertButton();
