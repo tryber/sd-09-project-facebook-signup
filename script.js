66ac6270a5492fdc90359d938dff7dde540712db
fernandacajueiro <fernandacajueiromachado@gmail.com>
2021-01-11 18:52:31 -0300

    Adiciona requisitos 7, 8, 9, 10 e 11

---
+++ script.js
@@ -0,0 +1,10 @@
+const entrarButton = document.getElementById('button-login');
+
+function clickButton() {
+  entrarButton.addEventListener('click', function () {
+  let loginName = document.getElementById('user-email-phone').value;
+  alert(`user ${loginName}`);
+});
+}
+
+clickButton();
