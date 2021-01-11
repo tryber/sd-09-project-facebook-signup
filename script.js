f290e27619fda87d507355e9116fd0f619272813
Matheus Schreiner <matheusrubisch@gmail.com>
2021-01-11 17:29:17 -0300

    Correçao codeClimate

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const btnEntrar = document.querySelector('#button-login');
 
 function addAlert() {
-  btnEntrar.addEventListener ('click', function() {
+  btnEntrar.addEventListener('click', function () {
     const valueEmail = document.querySelector('#user-email-phone');
     alert(valueEmail.value);
   });
