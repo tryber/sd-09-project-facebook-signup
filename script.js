d313ad20604438172c8c69bbbc81fff61fe3f9a1
Marcelo Campos <marcelo.campos.economista@gmail.com>
2021-01-11 20:06:02 -0300

    Adiciona o requisito 20 + correcoes codeclimate

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ const getInputs = document.querySelectorAll('#register-form input');
 const getRegisterBnt = document.querySelector('#facebook-register');
 const getButton = document.querySelector('#button-login');
 
-function checkEmptyFields() {
+const rightContent = document.querySelector('.right-content');
 
+function checkEmptyFields() {
   for (let index = 0; index < getInputs.length; index += 1) {
     if (getInputs[index].value === '') {
       alert('Campos inválidos');
@@ -11,11 +12,17 @@ function checkEmptyFields() {
   }
 }
 
+function validateGenderDiv() {
+  const genderDiv = document.querySelector('#personalized-gender');
+  if (genderDiv.innerHTML === '') {
+  }
+  return true;
+}
 function checkRadio() {
   const getRadioBtn = document.querySelectorAll('#radio-gender');
   const genderDiv = document.querySelector('#personalized-gender');
   for (let index = 0; index < getRadioBtn.length; index += 1) {
-    if (getRadioBtn[index].checked && validateGenderDiv()) {
+    if (getRadioBtn[index].checked && validateGenderDiv) {
       const personalizedGender = document.createElement('input');
       personalizedGender.name = 'gender-custom';
       personalizedGender.placeholder = 'Gênero (opcional)';
@@ -24,11 +31,20 @@ function checkRadio() {
   }
 }
 
-function validateGenderDiv() {
-  const genderDiv = document.querySelector('#personalized-gender');
-  if (genderDiv.innerHTML === '') {
-    return true;
-  }
+function insertWelcome() {
+  const firstName = document.querySelector('input[name="firstname"]');
+  const lastName = document.getElementsByName('lastname')[0];
+  const phoneEmail = document.getElementsByName('phone_email')[0];
+  const birthDate = document.getElementsByName('birthdate')[0];
+  const gender = document.querySelector('input[type="radio"]:checked');
+
+  const phrases = document.createElement('p');
+  phrases.innerHTML += `Ola, ${firstName.value} ${lastName.value} !`;
+  phrases.innerHTML += `<br> ${phoneEmail.value}`;
+  phrases.innerHTML += `<br> ${birthDate.value}`;
+  phrases.innerHTML += `<br> ${gender.value}`;
+  rightContent.innerHTML = '';
+  rightContent.appendChild(phrases);
 }
 
 getButton.addEventListener('click', function () {
@@ -37,6 +53,7 @@ getButton.addEventListener('click', function () {
 
 getRegisterBnt.addEventListener('click', function (event) {
   event.preventDefault();
-  checkEmptyFields;
-  checkRadio;
+  checkEmptyFields();
+  insertWelcome();
+  checkRadio();
 });
