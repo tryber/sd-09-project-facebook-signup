75bdb136f5bc35ec0f6437c72a63ee5d8f97463b
Kamilavrhcp <kamilavrhcp@hotmail.com>
2021-01-11 18:56:29 -0300

    lessons: acrescimo de campos em perzonalizados

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // Selectors
 const buttonLogin = document.querySelector('#button-login');
 const emailInput = document.querySelector('#user-email-phone');
-
+const customOption = document.querySelector('#custom-option')
+const inputsLabels = document.querySelector('.inputs-labels')
 // Function
 
 function alertEmailInput() {
@@ -12,3 +13,14 @@ function alertEmailInput() {
 // Events listener
 
 buttonLogin.addEventListener('click', alertEmailInput);
+
+function plusOption () {
+    const optionCustom = document.createElement('input');
+    optionCustom.type = 'text'
+    optionCustom.name = 'gender-custom';
+    optionCustom.placeholder = 'Gênero (opcional)';
+    inputsLabels.appendChild(optionCustom);
+    console.log('323223')
+}
+
+customOption.addEventListener('click', plusOption);
