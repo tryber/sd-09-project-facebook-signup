5a837065fca34b6d5df273ee2fbc147e6b04b685
Sabrina A <sasa2605.alves@gmail.com>
2021-01-11 16:59:55 -0300

    Requisitos 1 ao 6 feitos

---
+++ script.js
@@ -0,0 +1,6 @@
+let btnAlert = document.getElementById('button-login');
+function showAlert (e) {
+    
+    alert(document.getElementById('email-phone').value);
+}
+btnAlert.addEventListener('click', showAlert);
