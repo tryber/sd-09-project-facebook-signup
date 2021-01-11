fb4d20f6765781455d7901c1ee4dc84bbd06bff2
Osvaldo Borges <netoo70@gmail.com>
2021-01-11 19:22:53 -0300

    Requisito 19

--- script.js
+++ script.js
@@ -8,13 +8,26 @@ buttonLogin.addEventListener('click', alertInput);
 
 function genderCustom() {
   const father = document.querySelector('.radio-div');
-  const inputs = document.querySelectorAll('inputs')
-  if ()
+  const inputGender = document.querySelector('#gender-custom');
+  if (inputGender == null) {
     const input = document.createElement('input');
     input.placeholder = 'Gênero (opcional)';
     input.name = 'gender-custom';
+    input.id = 'gender-custom'
     father.appendChild(input);
+  }
 }
 
+function loopGender() {
+  const inputGender = document.querySelector('#gender-custom');
+  if (inputGender != null) {
+    inputGender.remove();
+  }
+}
+
+const buttonFeminino = document.querySelector('#feminino');
+buttonFeminino.addEventListener('click', loopGender);
+const buttonMasculino = document.querySelector('#masculino');
+buttonMasculino.addEventListener('click', loopGender);
 const buttonPersonalizado = document.querySelector('#personalizado');
 buttonPersonalizado.addEventListener('click', genderCustom);
