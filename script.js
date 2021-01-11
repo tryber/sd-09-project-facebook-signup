70e15d07d39ee5eee543addb96d299188bba9938
brenno-calado <brenno.calado@gmail.com>
2021-01-11 18:37:53 -0300

    Task 17

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const loginButton = document.getElementById('button-login');
+const registerButton = document.getElementById('facebook-register');
 
-loginButton.addEventListener('click', () => {
+loginButton.addEventListener('click', (evt) => {
+  evt.preventDefault();
   alert('Email ou telefone');
+});
+
+registerButton.addEventListener('click', (evt) => {
+  evt.preventDefault();
 });
