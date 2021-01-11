86071011fabd6016e3c301fa9f7ac47ab5140609
Thiago Carreira Vallim <thiagocvallim@gmail.com>
2021-01-11 18:02:23 -0400

    Corrige codeclimate

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const loginEmail = document.querySelector('#user-email-phone');
-const loginSenha = document.querySelector('#user-password');
 const buttonLogin = document.querySelector('#button-login');
 
 function getLogin() {
-  buttonLogin.addEventListener('click', function() {
+  buttonLogin.addEventListener('click', function () {
     window.alert(loginEmail.value);
-  })
+  });
 }
 
 getLogin();
