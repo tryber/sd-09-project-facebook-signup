5b5275aef1c70c4cc028dddac1239d078bf10900
gabriel mendonca <gabriel.dev.lop@gmail.com>
2021-01-11 17:59:20 -0300

    Corrigindo erros do code climate

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-window.onload = function () {
-  createEvents();
-};
+function verifyUserEmailPhone() {
+  const inputValue = document.querySelector('#user-email-phone');
+  alert(inputValue.value);
+}
 
 function createEvents() {
   const enter = document.querySelector('#button-login');
   enter.addEventListener('click', verifyUserEmailPhone);
 }
 
-function verifyUserEmailPhone() {
-  const inputValue = document.querySelector('#user-email-phone');
-  alert(inputValue.value);
-}
+window.onload = function () {
+  createEvents();
+};
