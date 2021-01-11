c0ef164bbe550ec1a3ae05dbd9252ee2bb8baa7b
Matheus Martino <matheus.martino97@gmail.com>
2021-01-11 17:00:08 -0300

    Adicionando o input de senha e atribuindo funcionalidade para o botao entrar.

---
+++ script.js
@@ -0,0 +1,13 @@
+function showFacebookLoginFormValues () {
+    const userEmailPhoneInput = document.querySelector('#user-email-phone')
+
+    window.alert(`E-mail ou Telefone: ${userEmailPhoneInput.value}`)
+}
+
+function listenToButtonLoginButton () {
+    const buttonLoginButton = document.querySelector('#button-login')
+
+    buttonLoginButton.addEventListener('click', showFacebookLoginFormValues)
+}
+
+listenToButtonLoginButton()
