346423d939569fc1511e2250bce92f030432e862
Leticia Lima <letylima96@gmail.com>
2021-01-11 20:57:35 -0300

    cria funcao para o botao cadastre-se

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ function alertEmailOrPhone() {
 
 const button = document.querySelector('#button-login');
 button.addEventListener('click', alertEmailOrPhone);
+
+function buttonSignUp() {
+  let emptyInputs = 0;
+  const inputs = document.getElementsByTagName('input');
+  const form = document.querySelector('.personal-infos');
+  const message = document.createElement('p');
+  form.appendChild(message);
+
+  for (let index = 0; index < inputs.length; index += 1) {
+    if (inputs[index].required.value === '') {
+        emptyInputs += 1;
+    }
+  }
+  if (emptyInputs > 0) {
+      message.innerText = 'Campos inválidos';
+  }
+}
+
+const btnSignUp = document.querySelector('#facebook-register');
+btnSignUp.addEventListener('click', buttonSignUp);
