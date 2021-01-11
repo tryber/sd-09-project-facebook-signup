158b1c0ca79eb7282646c85d27180360713b3413
Wendrick <wendrick.b.c1@gmail.com>
2021-01-11 17:09:14 -0300

    Requisito 16, 17, 19

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ buttonSubmit.addEventListener('click', () => {
   const userEmailPhone = document.querySelector('#user-email-phone');
   alert(userEmailPhone.value);
 });
+
+// Optional gender
+const gender = document.querySelectorAll('.gender');
+const input = document.querySelector('.optional-input');
+
+for (let i = 0; i < gender.length; i += 1) {
+  gender[i].addEventListener('click', () => {
+    if (gender[2].checked) {
+      input.style.display = 'block';
+    } else {
+      input.style.display = 'none';
+    }
+  });
+}
