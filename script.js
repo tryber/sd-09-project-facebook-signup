fa5ce5a1184ab1dce9f49835fd3de7cba472e189
Osvaldo Borges <netoo70@gmail.com>
2021-01-11 18:55:50 -0300

    Passando para o Robson

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ function alertInput() {
 
 const buttonLogin = document.querySelector('#button-login');
 buttonLogin.addEventListener('click', alertInput);
+
+function genderCustom() {
+  const father = document.querySelector('.radio-div');
+  const inputs = document.querySelectorAll('inputs')
+  if ()
+    const input = document.createElement('input');
+    input.placeholder = 'Gênero (opcional)';
+    input.name = 'gender-custom';
+    father.appendChild(input);
+}
+
+const buttonPersonalizado = document.querySelector('#personalizado');
+buttonPersonalizado.addEventListener('click', genderCustom);
