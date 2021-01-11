768bb4e9a305944678e683cfe5c58905ba64680c
lcgoandete <lcgoandete@hotmail.com>
2021-01-11 15:37:32 -0300

    concluido exercicio 7

---
+++ script.js
@@ -0,0 +1,5 @@
+const btnLogin = document.querySelector('#button-login');
+btnLogin.addEventListener('click', function () {
+  const userMailPhone = document.querySelector('#user-email-phone').value;
+  alert(userMailPhone);
+});
