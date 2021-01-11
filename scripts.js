0e2a4a2e25b6d826d8ba0a94a439626a2688b7c0
ederson rodrigo <edersonrodrigo31@gmail.com>
2021-01-11 19:01:38 -0300

    Setimo requisito code climate

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
 window.onload = function () {
-  const button = document.querySelector("#button-login");
+  const button = document.querySelector('#button-login');
 
-  const input = document.querySelector("#user-email-phone")
+  const input = document.querySelector('#user-email-phone');
 
-  button.addEventListener('click', function() {
-    return alert(input.value)
+  button.addEventListener('click', function () {
+    return alert(input.value);
   })
 }
