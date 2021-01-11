e7c8fa370f787762f83c547bbb24b040d038191f
Pablo Iago <evangelista.pabloiago@gmail.com>
2021-01-11 16:54:29 -0300

    Requisito 7

---
+++ script.js
@@ -0,0 +1,6 @@
+const loginButton = document.getElementById("button-login")
+
+loginButton.addEventListener("click",function() {
+  let typedEmail = document.getElementById("user-email-phone").value;
+  alert(typedEmail);
+})
