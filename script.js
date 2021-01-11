ae6489e77b46976ffe17126cf22e3e82bb967581
Poeta <pedrotrybe@gmail.com>
2021-01-11 18:43:27 -0300

    Requisito #18

--- script.js
+++ script.js
@@ -3,7 +3,33 @@ function alerta() {
   alert(email.value);
 }
 
+function validacao (event) {
+  if(document.getElementById('name').value === '' || document.getElementById('lastname').value === '' || document.getElementById('phonemail').value === '' || document.getElementById('senha').value === '' || document.getElementById('nascimento').value === '' ) {
+    alert('Campos inválidos');
+    event.preventDefault();
+  }
+  let status = false;
+  if (document.getElementById('fem').checked) {
+    status = true;
+  }
+
+  if (document.getElementById('masc').checked) {
+    status = true;
+  }
+
+  if (document.getElementById('perso').checked) {
+    status = true;
+  }
+
+  if (status === false) {
+    alert('Campos inválidos');
+    event.preventDefault();
+  }
+}
+
 window.onload = function () {
   const btn = document.querySelector('#button-login');
   btn.addEventListener('click', alerta);
+  const btnCadastro = document.querySelector('#facebook-register');
+  btnCadastro.addEventListener('click' , validacao);
 };
