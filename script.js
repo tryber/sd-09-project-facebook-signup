83a370e03fe0ed42b14c0e99554b37e440f1618b
LeoMajevski <leonardo.majevski@gmail.com>
2021-01-11 20:11:26 -0300

    Requisito 19

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 const buttonLogin = document.querySelector('#button-login');
 const emailOrPhone = document.querySelector('#user-email-phone');
+const maleRadio = document.querySelector('#male')
+const femaleRadio = document.querySelector('#female')
+const customGenderRadio = document.querySelector('#custom-gender')
+const customGenderInput = document.querySelector('#custom-gender-input');
+
 buttonLogin.addEventListener('click', function () {
   alert(emailOrPhone.value);
+});
+
+maleRadio.addEventListener('click', function () {
+  customGenderInput.classList.add('isVisible')
+});
+
+femaleRadio.addEventListener('click', function () {
+  customGenderInput.classList.add('isVisible')
+});
+
+customGenderRadio.addEventListener('click', function () {
+  customGenderInput.classList.remove('isVisible')
 });
