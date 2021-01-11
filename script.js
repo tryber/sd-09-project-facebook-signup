a69b4300be22fc733e0532795a735e96c8e62b5f
MarcioFujita <hanker78@gmail.com>
2021-01-11 17:52:30 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-// coment
+const button = document.getElementById('button-login');
+function falert(event) {
+    event.preventDefault();
+    const input = document.getElementById('user-email-phone');
+    const inputValue = input.value;
+    alert(inputValue);
+}
+button.addEventListener('click', falert);
