d48ae3712a249b1007db738c862407ace0b8b2f3
MarcosAlbuquerque <mpdsa20@gmail.com>
2021-01-11 20:24:22 -0300

    readicionado section, convençao de nomes e correçao do script do alert

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const loginButton = document.getElementById('button-login');
 const registerButton = document.getElementById('facebook-register');
+const loginField = document.getElementById('user-email-phone');
 
 loginButton.addEventListener('click', (evt) => {
   evt.preventDefault();
-  alert('Email ou telefone');
+  alert(loginField.value);
 });
 
 registerButton.addEventListener('click', (evt) => {
