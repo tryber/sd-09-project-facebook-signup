e951aced6945761761f3d1da97c5e5b2ef5bf264
fernandacajueiro <fernandacajueiromachado@gmail.com>
2021-01-11 19:01:20 -0300

    Altera alerta

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const entrarButton = document.getElementById('button-login');
 function clickButton() {
   entrarButton.addEventListener('click', function () {
     const loginName = document.getElementById('user-email-phone').value;
-    alert(`user ${loginName}`);
+    alert(`${loginName}`);
   });
 }
 
