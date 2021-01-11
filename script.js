6a321a6b69bd4bb60075d0c322bdbc14d827e783
lcgoandete <lcgoandete@hotmail.com>
2021-01-11 18:27:56 -0300

    refatorando exercicio 1

--- script.js
+++ script.js
@@ -3,3 +3,29 @@ btnLogin.addEventListener('click', function () {
   const userMailPhone = document.querySelector('#user-email-phone').value;
   alert(userMailPhone);
 });
+
+const btnCadastro = document.querySelector('#facebook-register');
+btnCadastro.addEventListener('click', function () {
+  const fields = document.querySelectorAll('.open-account input');
+
+  let campos = false;
+  for (const field of fields) {
+    if (field.type === 'text') {
+      if (field.value === '') {
+        break;
+      }
+    }
+
+    if (field.type === 'radio') {
+      if (field.checked) {
+        campos = true;
+      }
+    }
+  }
+
+  if (campos === false) {
+    const camposInvalidos = document.createElement('p');
+    camposInvalidos.innerText = 'Campos inválidos';
+    document.querySelector('.open-account').appendChild(camposInvalidos);
+  }
+});
