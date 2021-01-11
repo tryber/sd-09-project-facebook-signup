54586f2f7e99fbfeee197177cd0c888a96a3f307
Emanoel Leão <emanuelleaomarinho@gmail.com>
2021-01-11 18:31:29 -0300

    Ajustando ao Code Climate

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const btnEntrar = document.getElementById('button-login');
 const inputEmail = document.getElementById('user-email-phone');
-const inputPw = document.getElementById('user-password');
 
 function addlistener(obj, func) {
   obj.addEventListener('click', func);
