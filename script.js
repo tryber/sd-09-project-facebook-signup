e02f49bfebf52d32600998934d7061e0a7be8584
Layo Kaminski <layokaminski@gmail.com>
2021-01-11 17:07:03 -0300

    Requisito 7 - Ajustes do Code Climate

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const button = document.querySelector('#button-login');
 
 button.addEventListener('click', function () {
-  let inputUser = document.querySelector('#user-email-phone').value
-  alert(inputUser)
+  const inputUser = document.querySelector('#user-email-phone').value;
+  alert(inputUser);
 });
