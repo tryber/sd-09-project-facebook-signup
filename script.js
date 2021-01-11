63b0bbdc27a0fdc5413dde25283ae2c31c9ebe14
Ana Jacomassi <anajacomassip@gmail.com>
2021-01-11 19:23:59 -0300

    Adiciona requisito 7

---
+++ script.js
@@ -0,0 +1,5 @@
+const buttonLogin = document.querySelector('#button-login');
+buttonLogin.addEventListener('click', function () {
+  const caixaEmailTelefone = document.querySelector('#user-email-phone');
+  alert(caixaEmailTelefone.value);
+});
