5d52ea7abee9a53a88fd49d3a6fad893428c78e6
LeoMajevski <leonardo.majevski@gmail.com>
2021-01-11 20:13:22 -0300

    Corrige CC

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const buttonLogin = document.querySelector('#button-login');
 const emailOrPhone = document.querySelector('#user-email-phone');
-const maleRadio = document.querySelector('#male')
-const femaleRadio = document.querySelector('#female')
-const customGenderRadio = document.querySelector('#custom-gender')
+const maleRadio = document.querySelector('#male');
+const femaleRadio = document.querySelector('#female');
+const customGenderRadio = document.querySelector('#custom-gender');
 const customGenderInput = document.querySelector('#custom-gender-input');
 
 buttonLogin.addEventListener('click', function () {
@@ -10,13 +10,13 @@ buttonLogin.addEventListener('click', function () {
 });
 
 maleRadio.addEventListener('click', function () {
-  customGenderInput.classList.add('isVisible')
+  customGenderInput.classList.add('isVisible');
 });
 
 femaleRadio.addEventListener('click', function () {
-  customGenderInput.classList.add('isVisible')
+  customGenderInput.classList.add('isVisible');
 });
 
 customGenderRadio.addEventListener('click', function () {
-  customGenderInput.classList.remove('isVisible')
+  customGenderInput.classList.remove('isVisible');
 });
