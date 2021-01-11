1bec4252a1bfb00db36629147cca21cb26a74bff
Cristiano Lopes Duarte <tkdcris@gmail.com>
2021-01-11 17:30:00 -0300

    Requisito 7

---
+++ script.js
@@ -0,0 +1,12 @@
+const btnEntrar = document.querySelector('#button-login');
+const alertMsg = (event) => {
+  const textEmailTel = document.querySelector('#user-email-phone');
+  alert(textEmailTel.value);
+  event.preventDefault();
+}
+
+const eventslistener = () => {
+  btnEntrar.addEventListener('click', alertMsg);
+}
+
+eventslistener();
