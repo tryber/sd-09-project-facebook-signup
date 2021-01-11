5127f17f330047a1d86c72f173e31b5a672a0d1c
fernandacajueiro <fernandacajueiromachado@gmail.com>
2021-01-11 18:56:21 -0300

    Soluciona problemas do Code Climate

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const entrarButton = document.getElementById('button-login');
 
 function clickButton() {
   entrarButton.addEventListener('click', function () {
-  let loginName = document.getElementById('user-email-phone').value;
-  alert(`user ${loginName}`);
-});
+    const loginName = document.getElementById('user-email-phone').value;
+    alert(`user ${loginName}`);
+  });
 }
 
 clickButton();
