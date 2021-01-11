83d96e1a60585e2c42d43b08f2954331338040c8
Thiago Granville <thiagogranville@gmail.com>
2021-01-11 15:48:57 -0300

    Correções para o code climate

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 function alertButton() {
   const buttonLogin = document.querySelector('#button-login');
-
   buttonLogin.addEventListener('click', function () {
     const emailField = document.querySelector('#user-email-phone');
 
     alert(emailField.value);
   });
-
 }
 
 alertButton();
