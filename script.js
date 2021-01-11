91ea21d4b62f59d973434eb8327034fc0a234294
Tiago de Almeida Paz <tiagopaz.dev@gmail.com>
2021-01-11 17:03:22 -0300

    resolvendo code-climate

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const loginBtn = document.querySelector("#button-login");
-const emailPhoneValue = document.querySelector("#user-email-phone");
+const loginBtn = document.querySelector('#button-login');
+const emailPhoneValue = document.querySelector('#user-email-phone');
 
-loginBtn.addEventListener('click', function() {
+loginBtn.addEventListener('click', function () {
   alert(emailPhoneValue.value);
 });
