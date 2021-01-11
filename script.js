9f77dcc6df055c312cf8c1c3706769f65a91b9c8
Jhonatan Arguello <jhonatan.arguello@gmail.com>
2021-01-11 17:55:46 -0300

    Requisito 7. Inserindo o botao entrar e o alert ao clica-lo.

---
+++ script.js
@@ -0,0 +1,9 @@
+function logIn () {
+  const buttonListener = document.getElementById('button-login');
+
+  buttonListener.addEventListener("click", function(){
+    alert("Email ou telefone");
+  });
+}
+
+logIn()
