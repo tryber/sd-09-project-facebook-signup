8f65a7a1dbdaa0702d789691a89055fa0aff8cfe
Emanoel Leão <emanuelleaomarinho@gmail.com>
2021-01-11 18:28:52 -0300

    Alinhando o requisisto 7

--- script.js
+++ script.js
@@ -2,13 +2,12 @@ const btnEntrar = document.getElementById('button-login');
 const inputEmail = document.getElementById('user-email-phone');
 const inputPw = document.getElementById('user-password');
 
-console.log(btnEntrar);
 function addlistener(obj, func) {
-    obj.addEventListener('click', func);
+  obj.addEventListener('click', func);
 }
 
 function alertMsgLogIn() {
-    alert('Email:'+ inputEmail.value +' | Senha:' + inputPw.value);
+  alert(inputEmail.value);
 }
 
 addlistener(btnEntrar, alertMsgLogIn);
