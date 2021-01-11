94d6160fbd7d908a8d6e17161e19cdae88e8df03
MarceloPelegrino <pelegrinocorrea@gmail>
2021-01-11 16:36:53 -0300

    Requisito 7 dia 11-01-2021 16:36

---
+++ script.js
@@ -0,0 +1,5 @@
+let buttonLogin = document.querySelector('#button-login');
+let userEmailPhone = document.querySelector('#user-email-phone');
+buttonLogin.addEventListener('click', function() {
+    alert(userEmailPhone.value);
+});
