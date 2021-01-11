73a7f632b440a4c69ecaa84e9dd3204e3c810259
Cristiano Roberto <cristianoroberto@outlook.com>
2021-01-11 18:49:09 -0300

    20th Requirement

--- script.js
+++ script.js
@@ -22,6 +22,17 @@ function validateRadios(formInputs) {
   return isValid;
 }
 
+function genderSelected() {
+  let gender = "";
+  const genderRadioInputs = document.querySelectorAll('input[name="gender"]');
+  for (let index = 0; index < genderRadioInputs.length; index += 1) {
+    if (genderRadioInputs[index].checked) {
+      gender = genderRadioInputs[index].value;
+    }
+  } 
+  return gender;
+}
+
 function validateAndRegister(event) {
   const formInputs = document.querySelectorAll('.right-content input');
   let isTextValid = true;
@@ -32,15 +43,20 @@ function validateAndRegister(event) {
     event.preventDefault();
     const invalidFieldsParagraph = document.getElementById('invalid-fields');
     invalidFieldsParagraph.innerText = 'Campos inválidos';
+  } else {
+    const nome = document.getElementById('firstName').value;
+    const sobrenome = document.getElementById('lastName').value;
+    const phoneEmail = document.getElementById('phoneEmail').value;
+    const birthdate = document.getElementById('birthdate').value;    
+    const gender = genderSelected();
+
+    const rightContent = document.querySelector('.right-content');
+    rightContent.innerHTML = `
+    <p>Olá, ${nome} ${sobrenome}</p>
+    <p>Email ou Telefone: ${phoneEmail}</p>
+    <p>Data de nascimento: ${birthdate}</p>
+    <p>Gênero: ${gender}</p>`;
   }
 }
-
-const rightContent = document.querySelector('.right-content');
-  rightContent.innerHTML = `
-  <p>Olá, ${nome} ${sobrenome}</p>
-  <p>Email: ${email}</p>
-  <p>Data de nascimento: ${data}</p>
-  <p>Gênero: ${genero}</p>`;
-  
 const facebookRegister = document.getElementById('facebook-register');
 facebookRegister.addEventListener('click', validateAndRegister);
