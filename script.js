1d5930cb9cf8fcca8b79a315762795fa597bada3
Felipe Shcolnik <shcolnik@gmail.com.br>
2021-01-11 18:45:06 -0300

    ajuste de classes conforme manual

---
+++ script.js
@@ -0,0 +1,15 @@
+function checkEmail() {
+  const email = document.querySelector('.input-login');
+  let insertedEmail = email.value;
+  const emailFormat = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+$/.test(insertedEmail);
+  if (!emailFormat && insertedEmail.length) {
+    email.value = '';
+    alert('email inválido');
+    return false;
+  }
+  return emailFormat
+}
+
+window.onload = function () {
+
+}
