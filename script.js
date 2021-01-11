b4f7395127b1eb01cfb2dbf309ea72281f22903d
alailsonko <aalailson3@gmail.com>
2021-01-11 20:32:51 -0300

    add left container

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const button = document.getElementById('button-login');
 
 button.addEventListener('click', function () {
-  alert(document.getElementById('user-email-phone').value);
+  alert("Email ou telefone");
 });
