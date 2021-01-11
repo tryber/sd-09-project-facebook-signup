8ab285d34817f451b76068c14613122a9f932149
Giovani L D <giovani.diccini@hotmail.com>
2021-01-11 17:09:06 -0300

    ex7

---
+++ script.js
@@ -0,0 +1,8 @@
+function loginAlert() {
+  const loginBtn = document.getElementById('button-login');
+  loginBtn.addEventListener('click', function(){
+    alert(document.getElementById('user-email-phone').value);
+  });
+}
+
+loginAlert();
