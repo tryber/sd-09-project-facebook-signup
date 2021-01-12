95686f3e87b12dac2de63c125e24af5733a9488e
Caio <caioqs.info@gmail.com>
2021-01-12 10:59:05 -0300

    Req7- Criado funçao alerta.

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-// const a = 0;
+function showLogin(){
+  const buttonSubmit = document.querySelector('#button-login');
+  const emailPhone = document.querySelector('#user-email-phone');
+  buttonSubmit.addEventListener('click', function () {
+    alert(emailPhone.value);
+  });
+}
+
+showLogin();
