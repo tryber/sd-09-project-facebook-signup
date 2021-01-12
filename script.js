8d20dc012860efe7a49bb71a6d72bdd0819edb0d
RodrigoNeoEx <draja.acc@gmail.com>
2021-01-12 17:46:53 -0300

    function register para imprimir value

--- script.js
+++ script.js
@@ -14,8 +14,8 @@ function stopDefault(event) {
 const validateCamps = document.querySelectorAll('.cadastro input');
 function registerValidate() {
   for (let index = 0; index < validateCamps.length; index += 1) {
-    if (validateCamps[index].value === '') {
-      validateCamps[index].placeholder = 'Campos inválidos';
+    if (validateCamps[index].value === '') {      
+      validateCamps[index].value = 'Campos inválidos';
     }
   }
 }
