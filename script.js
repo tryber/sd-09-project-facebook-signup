fee8acfed806616b05f633c9dc83dde21fe2e3cf
Seu nome <seuemail@exemplo.br>
2021-01-12 01:43:16 -0300

    Alert

---
+++ script.js
@@ -0,0 +1,9 @@
+function alertEmailOrPhone() {
+    const inputEmailPhone = document.querySelector('#button-login');
+    const inputUserName = document.querySelector('#user-email-phone');
+    inputEmailPhone.addEventListener('click', function () {
+      alert(inputUserName.value);
+    });
+  }
+
+  alertEmailOrPhone();
