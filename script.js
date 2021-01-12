943465256e47d09a9ce46ef3f137ea2af921567a
brenno-calado <brenno.calado@gmail.com>
2021-01-11 23:38:51 -0300

    task 20 - showRegisteredData()

--- script.js
+++ script.js
@@ -8,6 +8,40 @@ const maleGender = document.getElementById('male');
 const femaleGender = document.getElementById('female');
 const allInputs = document.querySelectorAll('.input');
 const invalidMessage = document.createElement('p');
+const firstName = document.getElementById('first-name');
+const lastName = document.getElementById('last-name');
+const birthDate = document.getElementById('birthdate');
+const emailOrPhone = document.getElementById('phone-email');
+const rightSection = document.querySelector('.right-content');
+
+//Utilizei a lógica utilizada por Tiago Yoneda para fazer a função showRegisteredData e createFormDataResult
+
+function createFormDataResult(text) {
+  const formDataOutput = document.createElement('p');
+  formDataOutput.innerText = text;
+  rightSection.appendChild(formDataOutput);
+}
+
+function showRegisteredData() {
+  const helloUser = `Olá, ${firstName.value} ${lastName.value}`;
+  createFormDataResult(helloUser);
+  createFormDataResult(emailOrPhone.value);
+  createFormDataResult(birthDate.value);
+  createFormDataResult(document.querySelector('input[type="radio"]:checked').value);
+  rightSection.removeChild(registerForm);
+  rightSection.removeChild(document.querySelector('h1'));
+  rightSection.removeChild(document.querySelector('span'));
+}
+
+function checkValidity() {
+  for (let index = 0; index < allInputs.length; index += 1) {
+    if (!allInputs[index].checkValidity()) {
+      invalidMessage.innerText = 'Campos inválidos';
+      return registerForm.appendChild(invalidMessage);
+    }
+  }
+  return showRegisteredData();
+}
 
 loginButton.addEventListener('click', (evt) => {
   evt.preventDefault();
@@ -29,13 +63,4 @@ femaleGender.addEventListener('click', () => {
 
 maleGender.addEventListener('click', () => {
   customGenderInput.style.display = 'none';
-})
-
-function checkValidity() {
-  for (let index = 0; index < allInputs.length; index += 1) {
-    if (!allInputs[index].checkValidity()) {
-      invalidMessage.innerText = 'Campos inválidos';
-      return registerForm.appendChild(invalidMessage);
-    }
-  }
-}
+});
