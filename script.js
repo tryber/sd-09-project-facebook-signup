cba0d3a0a5b6274ff9193dcf01ebfecb07aa62f4
analuizams <analuizamsalgado@gmail.com>
2021-01-12 01:28:20 -0300

    requirement #11

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ const loginButton = document.getElementById('button-login');
 loginButton.addEventListener('click', function () {
   alert(document.getElementById('user-email-phone').value);
 });
+
+
+
+const personalizedGender = document.getElementById('gender-inputp');
+personalizedGender.addEventListener('click', function () {
+  const genderForm = document.getElementById('gender');
+  const newGenderInput = document.createElement('input');
+  newGenderInput.type = 'text';
+  newGenderInput.name = 'gender-custom';
+  newGenderInput.placeholder = 'Gênero (opcional)';
+  genderForm.appendChild(newGenderInput);
+}) 
