89e6c962103ae1449040b3666cd76f20eda8c85b
Luiz Henrique <lhfmworker@gmail.com>
2021-01-11 22:08:15 -0300

    validate requirement msg Campos Invalidos ae code climate2

--- script.js
+++ script.js
@@ -11,17 +11,17 @@ const eventslistener = () => {
 
 eventslistener();
 
-document.addEventListener("DOMContentLoaded", function() {
-  var elements = document.getElementsByTagName("input");
-  for (var i = 0; i < elements.length; i++) {
-      elements[i].oninvalid = function(e) {
-          e.target.setCustomValidity("");
-          if (!e.target.validity.valid) {
-              e.target.setCustomValidity("Campos inválidos");
-          }
-      };
-      elements[i].oninput = function(e) {
-          e.target.setCustomValidity("");
-      };
+document.addEventListener('DOMContentLoaded', function () {
+  const elements = document.getElementsByTagName('input');
+  for (let i = 0; i < elements.length; i += 1) {
+    elements[i].oninvalid = function (e) {
+      e.target.setCustomValidity('');
+      if (!e.target.validity.valid) {
+        e.target.setCustomValidity('Campos inválidos');
+      }
+    };
+    elements[i].oninput = function (e) {
+      e.target.setCustomValidity('');
+    };
   }
-})
+});
