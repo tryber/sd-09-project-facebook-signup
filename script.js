684c9ec2c4f497b30d3033900510aa1454ff3ad5
Ricardo Sbeghen <ricardo.sbeghen@gmail.com>
2021-01-12 11:58:20 -0300

    Ex 19 ..

--- script.js
+++ script.js
@@ -6,3 +6,27 @@ function loginAlert() {
 }
 
 loginAlert();
+
+function personalizeGender() {
+  const customGender = document.getElementById('customGender');
+  customGender.addEventListener('click', function() {
+    let textInput = document.createElement('input');
+    textInput.type = 'text';
+    textInput.name = 'gender-custom';
+    textInput.placeholder = 'Gênero (opcional)'
+
+    const genderInput = document.getElementById('genderInputDiv');
+
+    genderInput.appendChild(textInput);
+  });
+}
+
+personalizeGender();
+
+// function personalizeGender() {
+//   const customGender = document.getElementById('customGender');
+
+//   customGender.addEventListener('click', fuction() { 
+//     let newElement = document.createElement('input');
+//   });
+// }
