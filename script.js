f7186ca5048bed4ef23d3b48250c97871116bd11
pauloeliezerg <pauloeliezerg@gmail.com>
2021-01-12 01:14:13 -0300

    Added error alert for inputs with blank fields when clicking submit button

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 const submitBtn = document.getElementById('button-login');
 const userData = document.getElementById('user-email-phone');
+const registerBtn = document.getElementById('facebook-register');
+const formInputs = document.querySelectorAll('.right-content input');
 
 function loginAlert() {
   alert(userData.value);
 }
 
+function notNullRegisterValidation() {
+  for (let index = 0; index < formInputs.length; index += 1) {
+    if (formInputs[index].type === 'text' && formInputs[index].value === '') {
+      alert('Campos inválidos');
+      return;
+    }
+  }
+}
+
 submitBtn.addEventListener('click', loginAlert);
+registerBtn.addEventListener('click', notNullRegisterValidation);
