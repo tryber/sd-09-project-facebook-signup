0331ab5873f691dc46ae575956cd66c806a0ae55
Thiago Carreira Vallim <thiagocvallim@gmail.com>
2021-01-11 22:17:55 -0400

    Adiciona os requisitos 17 e 18

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const loginEmail = document.querySelector('#user-email-phone');
 const buttonLogin = document.querySelector('#button-login');
+const buttonSubmit = document.querySelector('#facebook-register');
 
 function getLogin() {
   buttonLogin.addEventListener('click', function () {
@@ -8,3 +9,35 @@ function getLogin() {
 }
 
 getLogin();
+
+function allRadioMade() {
+  const inputRadio = document.querySelectorAll('.inputRadio');
+  const isValide = false;
+  for (let index = 0; index < inputRadio; index += 1) {
+    if (inputRadio[index].checked) {
+      return true;
+    }
+  }
+  return isValide;
+}
+
+function allTextMade() {
+  const inputText = document.querySelectorAll('.inputText');
+  const isValide = true;
+  for (let index = 0; index < inputText; index += 1) {
+    if (inputText[index].value === '') {
+      return false;
+    }
+  }
+  return isValide;
+}
+
+function isValidate(event) {
+    if (!allTextMade() || !allRadioMade()) {
+      event.preventDefault();
+      const p = document.querySelector('#validate');
+      p.innerText = 'Campos inválidos';
+    }
+}
+
+buttonSubmit.addEventListener('click', isValidate);
