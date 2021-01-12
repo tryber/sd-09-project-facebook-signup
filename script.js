42aade7284758be4fcbcb50b0a2017ee015cdd04
MarcioFujita <hanker78@gmail.com>
2021-01-12 10:41:44 -0300

    Requisito 18

--- script.js
+++ script.js
@@ -6,3 +6,22 @@ function falert(event) {
   alert(inputValue);
 }
 button.addEventListener('click', falert);
+
+const btnCadastre = document.getElementById('facebook-register');
+function validInput(event) {
+  event.preventDefault();
+  const inputName = document.getElementById('name');
+  const inputLastname = document.getElementById('lastname');
+  const inputPhone = document.getElementById('phoneemail');
+  const inputNewpw = document.getElementById('newpassword');
+  const inputBirth = document.getElementById('birthdate');
+  const inputNameValue = inputName.value;
+  const inputLastnameValue = inputLastname.value;
+  const inputPhoneValue = inputPhone.value;
+  const inputNewpwValue = inputNewpw.value;
+  const inputBirthValue = inputBirth.value;
+  if (inputNameValue === ''|| inputLastnameValue === '' || inputPhoneValue === '' || inputNewpwValue === '' || inputBirthValue === '') {
+    alert('Campos inválidos');
+  }
+}
+btnCadastre.addEventListener('click', validInput);
