6acb9e917b8dbe96f00f79802abce906803a7024
Pedro Henrique <pedroenrique.400@gmail.com>
2021-01-11 21:16:54 -0300

    validando o code climate

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ const userEmailPhone = document.querySelector('#user-email-phone');
 const buttonFacebookRegister = document.querySelector('#facebook-register');
 const radioCustom = document.querySelector('#custom');
 const registerForm = document.querySelector('#register-form');
-
-buttonFacebookRegister.addEventListener('click', () => {
+//Faz a mensagem de erro caso os campos não estejam preenchidos
+buttonFacebookRegister.addEventListener('click', (event) => {
   const completeForm = document.querySelectorAll('#register-form input');
   for (let index = 0; index < completeForm.length; index += 1) {
     if (completeForm[index].value === '') {
-      let erroMensage = document.createElement('p');
+      const erroMensage = document.createElement('p');
       erroMensage.innerText = 'Campos Inválidos';
       erroMensage.style.color = 'red';
       erroMensage.style.fontSize = '15px';
@@ -17,10 +17,18 @@ buttonFacebookRegister.addEventListener('click', () => {
   }
   return 0;
 });
-
+// Faz o alert do email
 buttonLogin.addEventListener('click', (event) => {
   event.preventDefault();
   alert(userEmailPhone.value);
+});
+//faz o campo caso o gênero seja Personalizado
+radioCustom.addEventListener('click', () => {
+  const customOptionGenre = document.querySelector('#register-form .gender-custom');
+
+  if (customOptionGenre === null) {
+    registerForm.appendChild(createField());
+  }
 });
 
 function createField() {
@@ -31,11 +39,3 @@ function createField() {
 
   return inputText;
 }
-
-radioCustom.addEventListener('click', () => {
-  const registerSon = document.querySelector('#register-form .gender-custom');
-
-  if (registerSon === null) {
-    registerForm.appendChild(createField());
-  }
-});
