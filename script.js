fe61076847570292b760969ac786346430ad3800
Felipe Cardoso <felipedclc@gmail.com>
2021-01-12 13:43:01 -0300

    Validando todos os cam preenchidos ao clicar no botão Cadastre-se

--- script.js
+++ script.js
@@ -4,3 +4,40 @@ getButton.addEventListener('click', function () {
   const input = document.querySelector('#user-email-phone').value;
   alert(input);
 });
+
+function createMessageCamposInvalidos() {
+  const formCadastro = document.querySelector('#form-cadastro');
+  const p = document.createElement('p');
+  p.innerText = 'Campos inválidos';
+  p.style.border = '1px solid black';
+  p.id = 'invalid-msg';
+  formCadastro.appendChild(p);
+}
+
+
+function validatorRadio(inputs) {
+
+  for (let i = 0; i < inputs.length; i += 1) {
+    if (inputs[i].type === 'radio' && inputs[i].type === 'radio'.checked) {
+      return createMessageCamposInvalidos();
+    }
+  }
+}
+
+function validatorInputs(inputs) {
+  for (let i = 0; i < inputs.length; i += 1) {
+    if (inputs[i].value === '') return createMessageCamposInvalidos();
+  }
+}
+
+function verificaCamposCadastro() {
+  const inputs = document.querySelectorAll('#form-cadastro input');
+  const btnCadastro = document.querySelector('#facebook-register');
+
+  btnCadastro.addEventListener('click', function (e) {
+    e.preventDefault();
+    validatorRadio(inputs);
+    validatorInputs(inputs);
+  });
+}
+verificaCamposCadastro();
