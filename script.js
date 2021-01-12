d0d5c49fe07f6b48ebb279e40419c3ff37e65dc2
Henrique-Moura <henrique_hgm1996@hotmail.com>
2021-01-11 22:03:17 -0300

    Code

--- script.js
+++ script.js
@@ -7,30 +7,32 @@ buttonLogin.addEventListener('click', function () {
 
 function checkRadio() {
   const radioInput = document.querySelectorAll('.radio-container input');
+  let checkedInput;
   for (let index = 0; index < radioInput.length; index += 1) {
     if (radioInput[index].checked) {
-      return radioInput[index].value;
+      checkedInput = radioInput[index].value;
     }
   }
+  return checkedInput;
 }
 
-function checkInputValue () {
+function checkInputValue() {
   let check = false;
   const formInput = document.querySelectorAll('form>input');
-    for (let index = 0; index < formInput.length; index +=1) {
-      if (formInput[index].value === '' || checkRadio() === undefined) {
-        check = true;
-      }
+  for (let index = 0; index < formInput.length; index += 1) {
+    if (formInput[index].value === '' || checkRadio() === undefined) {
+      check = true;
     }
+  }
   return check;
 }
 
 window.onload = function () {
   const submitButton = document.querySelector('#facebook-register');
-  submitButton.addEventListener('click', function(event) {
+  submitButton.addEventListener('click', function (event) {
     event.preventDefault();
     if (checkInputValue()) {
       alert('Campos inválidos');
     }
   });
-}
+};
