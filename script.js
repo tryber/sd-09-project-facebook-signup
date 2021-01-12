026b528d479b8d4efb60b3e073356c2ae631ad73
MichaelMascarenhas <michaelmascarenhastrybe@gmail.com>
2021-01-12 18:06:35 -0300

    code climate

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 function buttonAlertLogin() {
-    const buttonLogin = document.querySelector('#button-login');
-    const valueEmail = document.querySelector('#user-email-phone');
-    buttonLogin.addEventListener('click', function () {
-        alert(valueEmail.value);
-    })
+  const buttonLogin = document.querySelector('#button-login');
+  const valueEmail = document.querySelector('#user-email-phone');
+  buttonLogin.addEventListener('click', function () {
+      alert(valueEmail.value);
+  });
 }
-
 buttonAlertLogin();
