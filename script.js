b7bf10dbc6d3273e6acaea445ac87b7d3aacc2bf
Henrique-Moura <henrique_hgm1996@hotmail.com>
2021-01-11 21:54:05 -0300

    Requisito 18

--- script.js
+++ script.js
@@ -4,3 +4,33 @@ buttonLogin.addEventListener('click', function () {
   const userEmailPhoneInputValue = userEmailPhoneInput.value;
   alert(userEmailPhoneInputValue);
 });
+
+function checkRadio() {
+  const radioInput = document.querySelectorAll('.radio-container input');
+  for (let index = 0; index < radioInput.length; index += 1) {
+    if (radioInput[index].checked) {
+      return radioInput[index].value;
+    }
+  }
+}
+
+function checkInputValue () {
+  let check = false;
+  const formInput = document.querySelectorAll('form>input');
+    for (let index = 0; index < formInput.length; index +=1) {
+      if (formInput[index].value === '' || checkRadio() === undefined) {
+        check = true;
+      }
+    }
+  return check;
+}
+
+window.onload = function () {
+  const submitButton = document.querySelector('#facebook-register');
+  submitButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    if (checkInputValue()) {
+      alert('Campos inválidos');
+    }
+  });
+}
