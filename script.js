58f8a4341dcd7453a66c4af123bf5f71ce1c05c6
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2021-01-12 19:06:04 -0300

    Code Climate issues

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-const button = document.querySelector('#button-login');
-const inputName = document.querySelector('#user-email-phone');
+const button = document.querySelector("#button-login");
+const inputName = document.querySelector("#user-email-phone");
 
-button.addEventListener('click', () => alert(inputName.value));
+button.addEventListener("click", () => alert(inputName.value));
 
-const custom = document.querySelector('#custom-gender');
-const customGender = document.querySelector('#pers');
+const custom = document.querySelector("#custom-gender");
+const customGender = document.querySelector("#pers");
 
-customGender.addEventListener('click', function () {
-  if(customGender.classList.contains('checked')) {
+customGender.addEventListener("click", function () {
+  if (customGender.classList.contains("checked")) {
     return;
-}
-  const newGender = document.createElement('input');
-  newGender.placeholder = 'Gênero (opcional)';
+  }
+  const newGender = document.createElement("input");
+  newGender.placeholder = "Gênero (opcional)";
   custom.appendChild(newGender);
-  customGender.classList = 'checked';
-})
+  customGender.classList = "checked";
+});
