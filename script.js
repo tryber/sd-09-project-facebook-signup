100d0b8b6d5d94f6e8b4b2dadad65df34eba3cd7
LucioOSilva <lucioosilva00@gmail.com>
2021-01-11 23:17:23 -0300

    base projeto facebook - dupla: Caio Queiroz

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-// const a = 0;
+function alertEmailOrPhone() {
+  const inputEmailPhone = document.querySelector('#button-login');
+  const inputUserName = document.querySelector('#user-email-phone');
+  inputEmailPhone.addEventListener('click', function () {
+    alert(inputUserName.value);
+  });
+
+}
+
+function validateForm() {
+  const formSubscribe = document.querySelector('#form-subscribe');
+  const formSubscribeSubmitButton = document.querySelector('#facebook-register');
+  formSubscribeSubmitButton.addEventListener('click', function (event) {
+    event.preventDefault();
+    if (!formSubscribe.checkValidity()) {
+      formSubscribe.classList.add('nova');
+    } else {
+    formSubscribe.classList.remove('nova');
+    }
+  });
+}
+
+validateForm();
+alertEmailOrPhone();
