d56d2b9c8ce1d2ef00972c31ea9eb8811d656da6
saulokirchmaier <saulokirchmaier@gmail.com>
2021-01-12 11:55:10 -0300

    Show optional gender

--- script.js
+++ script.js
@@ -2,18 +2,28 @@ const email = document.querySelector('#user-email-phone');
 const botao = document.querySelector('#button-login');
 const botaoCadastro = document.querySelector('#facebook-register');
 const form1 = document.querySelector('.form-register');
-const errorParagraph = document.createElement('p');
-errorParagraph.classList.add('message-error');
-form1.appendChild(errorParagraph);
+const paragrafoErro = document.createElement('p');
+paragrafoErro.classList.add('message-error');
+form1.appendChild(paragrafoErro);
+const generoPersonalizado = document.querySelector('#gender-custom');
+const sessaoGenero = document.querySelector('.gender');
 
 botao.addEventListener('click', () => alert(email.value));
 botaoCadastro.addEventListener('click', (event) => {
   event.preventDefault();
-  if (errorParagraph.innerHTML) errorParagraph.innerHTML = '';
+  if (paragrafoErro.innerHTML) paragrafoErro.innerHTML = '';
   const mensagemErro = document.createTextNode('Campos inválidos');
   // Baseado no código do Hugo Braga da turma 8
   const resultado = form1.checkValidity();
   if (!resultado) {
-    errorParagraph.appendChild(mensagemErro);
+    paragrafoErro.appendChild(mensagemErro);
   }
 });
+
+function showOptionalGender(event) {
+  if (event.target.id === 'personalizado' && event.target.checked) {
+    if (generoPersonalizado.style.display === 'none') generoPersonalizado.style.display = 'block';
+  } else generoPersonalizado.style.display = 'none';
+}
+
+sessaoGenero.addEventListener('click', showOptionalGender);
