7b3e09a7478ebbbebbb5dd9bc1a69f871a1d548c
Henrique Brito Elias <henriquebelias@gmail.com>
2021-01-12 09:58:37 -0300

    Requisito 19 - Dia 12-01-2021 09:58

--- script.js
+++ script.js
@@ -4,13 +4,22 @@ buttonLogin.addEventListener('click', function () {
   alert(userEmailPhone.value);
 });
 
-const facebookRegister = document.querySelector('#facebook-register');
-const inputs = document.querySelectorAll('input');
-facebookRegister.addEventListener('click', function (event) {
-  event.preventDefault();
-  for (let index = 2; index < inputs.length; index += 1) {
-    if (inputs[index].value === '') {
-      inputs[index].value = 'Campos inválidos';
-    }
-  }
-});
+// const facebookRegister = document.querySelector('#facebook-register');
+// const inputs = document.querySelectorAll('input');
+// facebookRegister.addEventListener('click', function (event) {
+//   event.preventDefault();
+//   for (let index = 2; index < inputs.length; index += 1) {
+//     if (inputs[index].value === '') {
+//       inputs[index].value = 'Campos inválidos';
+//     }
+//   }
+// });
+
+const customGender = document.querySelector('#custom-gender');
+const personalizado = document.querySelector('#personalizado');
+personalizado.addEventListener('click', function () {
+  const input = document.createElement('input');
+  input.name = 'gender-custom';
+  input.placeholder = 'Gênero (opcional)';
+  customGender.appendChild(input);
+})
