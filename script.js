cb65b3949b972792a063f0f098a82995ddec2462
Cristiano Lopes Duarte <tkdcris@gmail.com>
2021-01-11 22:15:25 -0300

    Iniciando script de validaçao customizada de genero

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 const btnEntrar = document.querySelector('#button-login');
+
 const alertMsg = (event) => {
   const textEmailTel = document.querySelector('#user-email-phone');
   alert(textEmailTel.value);
   event.preventDefault();
 };
 
+const customGenre = () => {
+  const customChoose = document.querySelector('#personalizado');
+  if ()
+}
+
 const eventslistener = () => {
   btnEntrar.addEventListener('click', alertMsg);
+  btnEntrar.addEventListener('click', customGenre);
 };
 
-eventslistener();
+window.onload = () => {
+  eventslistener();
+}
+
