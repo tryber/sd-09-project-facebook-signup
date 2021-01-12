d92f71ee4e8341e7a3553ddfb0a0bb887ac3c5be
Emanoel Leão <emanuelleaomarinho@gmail.com>
2021-01-12 00:59:07 -0300

    Requisito 18

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+const btnReg = document.getElementById('facebook-register');
 const btnEntrar = document.getElementById('button-login');
 const inputEmail = document.getElementById('user-email-phone');
+const listInputRadioCad = document.getElementsByClassName('isRadio');
+const listImputNoRadioCad = document.getElementsByClassName('forms-cad');
 
 function addlistener(obj, func) {
   obj.addEventListener('click', func);
@@ -9,4 +12,34 @@ function alertMsgLogIn() {
   alert(inputEmail.value);
 }
 
+function isEmpty(obj){
+  for(let index = 0; index < obj.length; index +=1) {
+    if(obj[index].value == "") {
+      return true;
+    }
+  }
+  return false;
+}
+
+function isCheck(obj) {
+  for(let index = 0; index < obj.length; index +=1) {
+    if(obj[index].checked) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function verifyInputValue(event) {
+  event.preventDefault();
+
+  if(isEmpty(listImputNoRadioCad)){
+    alert('Campos inválidos');
+  } else if (!isCheck(listInputRadioCad)){
+    alert('Campos inválidos');
+  }
+}
+
+
 addlistener(btnEntrar, alertMsgLogIn);
+addlistener(btnReg ,verifyInputValue)
