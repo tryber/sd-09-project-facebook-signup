faecb6f87247e4b26f40ac732db16d50a2ece74b
Marcelo Campos <marcelo.campos.economista@gmail.com>
2021-01-12 10:28:52 -0300

    Consertando insues code climate

--- script.js
+++ script.js
@@ -1,22 +1,11 @@
 const getInputs = document.querySelectorAll('#register-form input');
 const getRegisterBnt = document.querySelector('#facebook-register');
 const getButton = document.querySelector('#button-login');
-const getRadioBtn = document.querySelectorAll('input[type="radio"]');
 
 const personalizedInput = document.querySelector('#personalized');
-const genderDiv = document.querySelector('#personalized-gender')
+const genderDiv = document.querySelector('#personalized-gender');
 
 const rightContent = document.querySelector('.right-content');
-
-function checkEmptyFields() {
-  for (let index = 0; index < getInputs.length; index += 1) {
-    if (getInputs[index].value === '') {
-      getInputs[index].innerText = 'Campos inválidos';
-    } else {
-      insertWelcome();
-    }
-  }
-}
 
 function validateGenderDiv() {
   const genderDiv = document.querySelector('#personalized-gender');
@@ -40,17 +29,28 @@ function insertWelcome() {
   rightContent.appendChild(phrases);
 }
 
+function checkEmptyFields() {
+  for (let index = 0; index < getInputs.length; index += 1) {
+    if (getInputs[index].value === '') {
+      getInputs[index].innerText = 'Campos inválidos';
+    } else {
+      insertWelcome();
+    }
+  }
+}
+
 getButton.addEventListener('click', function () {
   alert(document.querySelector('#user-email-phone').value);
 });
 
 personalizedInput.addEventListener('click', function () {
-  // if (validateGenderDiv === true) {}
-  const personalizedGender = document.createElement('input');
-  personalizedGender.type = 'text';
-  personalizedGender.name = 'gender-custom';
-  personalizedGender.placeholder = 'Gênero (opcional)';
-  genderDiv.appendChild(personalizedGender);
+  if (validateGenderDiv) {
+    const personalizedGender = document.createElement('input');
+    personalizedGender.type = 'text';
+    personalizedGender.name = 'gender-custom';
+    personalizedGender.placeholder = 'Gênero (opcional)';
+    genderDiv.appendChild(personalizedGender);
+  }
 });
 
 getRegisterBnt.addEventListener('click', function (event) {
