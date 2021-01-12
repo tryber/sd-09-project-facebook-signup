f3154894873a3482b1f11f1fcfb9a4daf9f7f0cc
RodrigoNeoEx <draja.acc@gmail.com>
2021-01-12 14:02:15 -0300

    climate

--- script.js
+++ script.js
@@ -26,33 +26,7 @@ function listeners() {
   registerButton.addEventListener('click', registerValidate);
 
 }
-// utlizar function abaixo como base para checkar todos campos
-// function confirmAllPersonalData() {  
-//   const cvSection = document.getElementById('checkCV'); 
-//   let getInput = document.querySelectorAll('.one input, select');
 
-//   for(let index = 0; index < getInput.length; index += 1) {   
-//     let div = document.createElement('div');
-//     div.innerHTML = `${getInput[index].name}: ${getInput[index].value}`
-//     cvSection.appendChild(div)
-//   }
-//   if(getInput[4].checked === true) {
-//     cvSection.removeChild(cvSection.children[5])
-//   }
-//   if(getInput[5].checked === true) {
-//     cvSection.removeChild(cvSection.children[4])
-//   }
-// }
-// function confirmAllPersonalProfile() {
-//   const cvSection = document.getElementById('checkCV'); 
-//   let getInput = document.querySelectorAll('.two input, textarea');
-//   for(let index = 0; index < getInput.length; index += 1) {
-//     let div = document.createElement('div');
-//     div.innerHTML = `${getInput[index].name}: ${getInput[index].value}`
-//     cvSection.appendChild(div)
-//   }
-// }
-// 
 // function checkEmail() {
 //   const email = document.querySelector('.input-login');
 //   let insertedEmail = email.value;
