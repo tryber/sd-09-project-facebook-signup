952651f7cd2d753e23f8ac72369d962e55a6e6f7
Luiz Henrique <lhfmworker@gmail.com>
2021-01-11 22:03:30 -0300

    validate requirement msg Campos Invalidos

--- script.js
+++ script.js
@@ -10,3 +10,18 @@ const eventslistener = () => {
 };
 
 eventslistener();
+
+document.addEventListener("DOMContentLoaded", function() {
+  var elements = document.getElementsByTagName("input");
+  for (var i = 0; i < elements.length; i++) {
+      elements[i].oninvalid = function(e) {
+          e.target.setCustomValidity("");
+          if (!e.target.validity.valid) {
+              e.target.setCustomValidity("Campos inválidos");
+          }
+      };
+      elements[i].oninput = function(e) {
+          e.target.setCustomValidity("");
+      };
+  }
+})
