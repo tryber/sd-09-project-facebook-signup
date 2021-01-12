1f0b881d3d8958cd593d947d36a03b85c3ef5d71
Pablo Fernandes <pabloprates@gmail.com>
2021-01-12 10:19:15 -0300

    Adding a button Entrar behavior -- CC Round 1

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-btnEntrar = document.getElementById('button-login');
-btnEntrar.addEventListener('click', showLoginData);
-
 function showLoginData(evt) {
   evt.preventDefault();
   const loginEmailPhone = document.getElementById('user-email-phone').value;
   alert(loginEmailPhone);
 }
+
+const btnEntrar = document.getElementById('button-login');
+btnEntrar.addEventListener('click', showLoginData);
