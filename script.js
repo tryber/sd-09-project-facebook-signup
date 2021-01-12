a8d1b0bddf848d2a11d4feea73555500b799583a
brunamfguimaraes <brunamfguimaraes@gmail.com>
2021-01-12 18:54:28 -0300

    Adicionando requisito 19

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ const button = document.querySelector('#button-login');
 const inputName = document.querySelector('#user-email-phone');
 
 button.addEventListener('click', () => alert(inputName.value));
+
+const custom = document.querySelector('#custom-gender');
+const customGender = document.querySelector('#pers');
+
+customGender.addEventListener('click', function () {
+  if(customGender.classList.contains('checked')) {
+    return;
+}
+  const newGender = document.createElement('input');
+  newGender.placeholder = 'Gênero (opcional)';
+  custom.appendChild(newGender);
+  customGender.classList = 'checked';
+})
