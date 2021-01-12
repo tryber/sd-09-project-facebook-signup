4197f96f2ce55a5f1f02490e3e3b6584f4cfb9b4
MichaelMascarenhas <michaelmascarenhastrybe@gmail.com>
2021-01-12 18:02:43 -0300

    alert quando clicar no button Entrar

---
+++ script.js
@@ -0,0 +1,9 @@
+function buttonAlertLogin() {
+    const buttonLogin = document.querySelector('#button-login');
+    const valueEmail = document.querySelector('#user-email-phone');
+    buttonLogin.addEventListener('click', function () {
+        alert(valueEmail.value);
+    })
+}
+
+buttonAlertLogin();
