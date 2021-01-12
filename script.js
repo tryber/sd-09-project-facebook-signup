7788d36d85b3ee483dc351127e0dfcde61804343
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2021-01-12 17:42:19 -0300

    ajustando funcao para arrow function

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 const button = document.querySelector('#button-login');
 const inputName = document.querySelector('#user-email-phone');
 
-button.addEventListener('click', function () {
-  alert(inputName.value);
-});
+button.addEventListener('click', () => alert(inputName.value));
