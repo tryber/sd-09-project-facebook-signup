1f4f38b44821f7ef4416aede0ab64da64df18142
edison-ps <edison@osite.com.br>
2021-01-12 09:16:55 -0300

    Javascript alert V1.00

---
+++ script.js
@@ -0,0 +1,14 @@
+/* Variaveis e constantes */
+const inputEmailPhone = document.querySelector('#user-email-phone');
+const buttonSender = document.querySelector('#button-login');
+
+/* Funcoes */
+
+/* Inicio do codigo */
+
+/* Exibe e-mail-telefone */
+buttonSender.addEventListener('click', function() {
+  if (inputEmailPhone.value  !== '') {
+      alert(inputEmailPhone.value);
+  }
+});
