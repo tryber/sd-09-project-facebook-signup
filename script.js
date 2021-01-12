6b2f7cc32298459b83aa6be0849538d9cb3e0ed5
Marina Alane <marina.alane@outlook.com>
2021-01-12 19:23:17 -0300

    ajusta issues cc

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function clicar() {
   const btnclicar = document.querySelector('#button-login');
   const emailPhone = document.querySelector('#user-email-phone');
   btnclicar.addEventListener('click', function () {
-    alert( emailPhone.value);
+    alert(emailPhone.value);
   });
 }
 
