a37ad0d0c156b8a4d2d4588327540e0691c2b9e4
Sabrina A <sasa2605.alves@gmail.com>
2021-01-12 16:01:04 -0300

    Update required evaluator

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const btnAlert = document.getElementById('button-login');
 
 function showAlert() {
-  alert(document.getElementById('user-email-phone-label').value);
+  alert(document.getElementById('user-email-phone').value);
 }
 btnAlert.addEventListener('click', showAlert);
