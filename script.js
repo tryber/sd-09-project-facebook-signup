d1a60ac2c9213ab094ea60d80e7eb1326db3d002
Igor Marcos Riegel <igor.riegel@gmail.com>
2021-01-12 19:03:21 -0300

    criada validaçao de campos e movidos os radio buttons para labels proprios.

--- script.js
+++ script.js
@@ -4,3 +4,32 @@ loginButton.addEventListener('click', function () {
   const typedEmail = document.getElementById('user-email-phone').value;
   alert(typedEmail);
 });
+
+const registerButton = document.getElementById('facebook-register');
+
+function validateInputBoxes() {
+  const myForm = document.getElementsByTagName("input");
+  let fieldsAreInvalid = false;
+  for(let index = 2; index <= 6; index +=1) {
+    fieldsAreInvalid = (myForm[index].value === "");
+    if (fieldsAreInvalid) {
+      return true;
+    };
+  }
+}
+
+function validateRadioButtons() {
+  const myForm = document.getElementsByTagName("input");
+  let fieldsAreInvalid = Boolean(myForm[7].checked || myForm[8].checked || myForm[9].checked);
+  return !fieldsAreInvalid;
+}
+
+function validateForm() {
+  let areFieldsInvalid = validateInputBoxes();
+  let areRadioBttnsInvalid = validateRadioButtons();
+  if (areFieldsInvalid || areRadioBttnsInvalid) {
+    alert('Campos inválidos');
+  }
+}
+
+registerButton.addEventListener('click', validateForm);
