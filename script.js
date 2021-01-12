dd3f14c144455aa61f2babfdc6892c7438d411af
Felipe Rocha <felipe.sr27@gmail.com>
2021-01-11 21:12:17 -0300

    Requirement 19: added function that verify empty fields in register form.

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 const buttonEntrar = document.getElementById('button-login');
+const registerButton = document.getElementById('facebook-register');
 
 buttonEntrar.addEventListener('click', (event) => {
   event.preventDefault();
   const emailOrPhone = document.getElementById('user-email-phone').value;
   alert(emailOrPhone);
+});
+
+registerButton.addEventListener('click', (event) => {
+  const formContainer = document.getElementsByClassName('form-wrapper');
+  for (let index = 0; index < formContainer.length; index += 1) {
+    for (let index_2 = 0; index < formContainer[index].childNodes.length; index += 1) {
+      if (formContainer[index].childNodes[index].value !== undefined) {
+        if (formContainer[index].childNodes[index].value === ''){
+          alert('Campos inválidos');
+          event.preventDefault();
+        }
+      }
+    }
+  }
 });
