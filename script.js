c91868d771e5f3410bd9399dba72a75450be1d67
Maximilian Kaden <mkadenfilgo@gmail.com>
2021-01-12 19:53:37 -0300

    Requisito 18

--- script.js
+++ script.js
@@ -16,9 +16,44 @@ function showTextInput() {
   inputFieldDiv.appendChild(inputField);
 }
 
+function validateRegistration() {
+  const formData = document.querySelectorAll('#new-account-form > input');
+  if (validateGender()) {
+    for (let index = 0; formData.length > index; index += 1) {
+      console.log(formData[index].value)
+      if (formData[index].value === ''){
+        showFieldsAreInvalid()
+        return
+      }
+    }
+  }
+  console.log('Dados Validos')
+}
+
+function validateGender(){
+  const radiosButtons = document.querySelectorAll('#radio-buttons-form > input');
+  console.log("Fazendo verificação dos radios buttons")
+  for (let index = 0; radiosButtons.length > index; index += 1) {
+    if (radiosButtons[index].checked === true){
+      return true
+    }
+  }
+  showFieldsAreInvalid()
+}
+
+function showFieldsAreInvalid() {
+  const formData = document.querySelector('#new-account-form');
+  let div = document.createElement('div');
+  div.textContent = 'Campos inválidos';
+  formData.appendChild(div);
+}
+
 function addListeners() {
   const personalizadoButton = document.querySelector('#personalizado-button');
   personalizadoButton.addEventListener('click', showTextInput);
+
+  const registrationButton = document.querySelector('#facebook-register');
+  registrationButton.addEventListener('click', validateRegistration);
 }
 
 addListeners();
