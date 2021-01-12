ddca61d6a30fe1d7ee63cafc64f46a54eda386cc
smylle <jjsilveira007@gmail.com>
2021-01-11 23:07:22 -0300

    Corrige funcão verificadora do form

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const loginButton = document.querySelector('#button-login');
 const loginInput = document.querySelector('#user-email-phone');
 const registerButton = document.querySelector('#facebook-register');
-const formInputs = document.querySelectorAll('countInput');
+const formInputs = document.querySelectorAll('.countInput');
+const alertP = document.querySelector('#alertP');
+let validation = 0;
 
 function showAlert() {
   loginButton.addEventListener('click', function () {
@@ -11,10 +13,15 @@ function showAlert() {
 showAlert();
 
 function verifyForm () {
-  registerButton.addEventListener('click', function () {
+  registerButton.addEventListener('click', function (event) {
+    event.preventDefault();
     for (let index = 0; index < formInputs.length; index += 1) {
-      if (formInputs[index].value = 'undefined') {
-        alert('Campos inválidos');
+      if (formInputs[index].type === 'radio' && !formInputs[index].checked) {
+        validation = validation +1;
+      }
+      if (formInputs[index].value === '' || validation > 2) {
+        alertP.innerText = ('Campos inválidos');
+        break;
       }
     }
   });
