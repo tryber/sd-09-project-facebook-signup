ccf771892975bf4bf5125c67dc8f9ff13805093a
Thiago Granville <thiagogranville@gmail.com>
2021-01-12 19:09:45 -0300

    Requisito 20: Substitua o conteúdo do container com a classe right-content se o formulário estiver completamente preenchido e validado

--- script.js
+++ script.js
@@ -7,12 +7,62 @@ function alertButton() {
   });
 }
 
+function personalizeGenderField() {
+  const personalizedField = document.querySelector('#per-radio');
+
+  personalizedField.addEventListener('click', function () {
+    if (personalizedField.checked) {
+      const personalizedContainer = document.querySelector('#personalized-gender-container');
+      const inputPersonalizedGender = document.createElement('input');
+      inputPersonalizedGender.type = 'type="text"';
+      inputPersonalizedGender.name = 'gender-custom';
+      inputPersonalizedGender.placeholder = 'Gênero (opcional)';
+      personalizedContainer.appendChild(inputPersonalizedGender);
+    }
+  });
+}
+
 function submitInfo() {
   const rightContent = document.querySelector('.right-content');
+  const nameValue = document.querySelector('.names-container input:nth-child(1)').value;
+  const surnameValue = document.querySelector('.names-container input:nth-child(2)').value;
+  const emailValue = document.querySelector('.register-form>input:nth-child(2)').value;
+  const birthdateValue = document.querySelector('.register-form>input:nth-child(5)').value;
+  let gender = undefined;
+  if (document.querySelector('#personalized-gender-container input')) {
+    gender = document.querySelector('#personalized-gender-container input').value;
+  } else {
+    const femaleField = document.querySelector('#fem-radio');
+    const maleField = document.querySelector('#masc-radio');
 
+    if (femaleField.checked) {
+      gender = femaleField.value;
+    } else if (maleField.checked) {
+      gender = maleField.value;
+    } 
+  }
+ 
+  const greeting = document.createElement('p');
+  greeting.innerText = `Olá, ${nameValue} ${surnameValue}`
+
+  const email = document.createElement('p');
+  email.innerText = `${emailValue}`
+
+  const birthdate = document.createElement('p');
+  birthdate.innerText = `${birthdateValue}`
+
+  const chosenGender = document.createElement('p');
+  chosenGender.innerText = `${gender}`
+ 
+  
   while (rightContent.firstChild) {
     rightContent.removeChild(rightContent.firstChild);
   }
+
+  rightContent.appendChild(greeting);
+  rightContent.appendChild(email);
+  rightContent.appendChild(birthdate);
+  rightContent.appendChild(chosenGender);
 }
 
 function validateField() {
@@ -39,21 +89,6 @@ function validateField() {
   });
 }
 
-function personalizeGenderField() {
-  const personalizedField = document.querySelector('#per-radio');
-
-  personalizedField.addEventListener('click', function () {
-    if (personalizedField.checked === true) {
-      const personalizedContainer = document.querySelector('#personalized-gender-container');
-      const inputPersonalizedGender = document.createElement('input');
-      inputPersonalizedGender.type = 'type="text"';
-      inputPersonalizedGender.name = 'gender-custom';
-      inputPersonalizedGender.placeholder = 'Gênero (opcional)';
-      personalizedContainer.appendChild(inputPersonalizedGender);
-    }
-  });
-}
-
-personalizeGenderField();
+personalizeGenderField() 
 validateField();
 alertButton();
