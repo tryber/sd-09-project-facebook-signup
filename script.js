6cceae0ae49735a265e48c140b7fde293191ce7f
Raphael Rêgo <raphael.mlrego@gmail.com>
2021-01-12 15:42:03 -0300

    requisito 20

--- script.js
+++ script.js
@@ -3,11 +3,22 @@ btnLogin.addEventListener('click', function () {
   const userMailPhone = document.querySelector('#user-email-phone').value;
   alert(userMailPhone);
 });
-
+const fullName = document.createElement('p');
+const phoneEmail = document.createElement('p');
+const birthdate = document.createElement('p');
+const gender2 = document.createElement('p');
+function addContent() {
+  fullName.innerText = `Olá, ${document.querySelector('#firstname').value} ${document.querySelector('#lastname').value}`;
+  phoneEmail.innerText = `Telefone/Email: ${document.querySelector('#phone_email').value}`;
+  birthdate.innerText = `Data de Nascimento: ${document.querySelector('#birthdate').value}`;
+  document.querySelector('.right-content').appendChild(fullName);
+  document.querySelector('.right-content').appendChild(phoneEmail);
+  document.querySelector('.right-content').appendChild(birthdate);
+  document.querySelector('.right-content').appendChild(gender2);
+}
 const btnCadastro = document.querySelector('#facebook-register');
 btnCadastro.addEventListener('click', function (event) {
   const fields = document.querySelectorAll('.open-account input');
-
   let campos = false;
   for (let field = 0; field < fields.length; field += 1) {
     if (fields[field].type === 'text') {
@@ -19,6 +30,7 @@ btnCadastro.addEventListener('click', function (event) {
     if (fields[field].type === 'radio') {
       if (fields[field].checked) {
         campos = true;
+        gender2.innerText = `Gênero: ${fields[field].value}`;
       }
     }
   }
@@ -28,6 +40,13 @@ btnCadastro.addEventListener('click', function (event) {
     const camposInvalidos = document.createElement('p');
     camposInvalidos.innerText = 'Campos inválidos';
     document.querySelector('.open-account').appendChild(camposInvalidos);
+  }
+  if (campos === true) {
+    event.preventDefault();
+    for (let index = 0; index <= document.querySelector('.right-content').children.length - 1; index += 1) {
+      document.querySelector('.right-content').children[index].style.display = 'none';
+    }
+    addContent();
   }
 });
 
