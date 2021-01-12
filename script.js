f48438c7e970f077830f384489de229b9da6a3d2
analuizams <analuizamsalgado@gmail.com>
2021-01-12 04:26:16 -0300

    add requirement 20

--- script.js
+++ script.js
@@ -20,13 +20,30 @@ personalizedGender.addEventListener('click', function () {
 function validateForm(event) {
   event.preventDefault();
   const requiredItems = document.querySelectorAll('.required');
+  const checkedInput = document.querySelector('input[type="radio"]:checked');
   const validateMessage = document.getElementById('validate-message');
+  let validate = true;
   for (let index = 0; index < requiredItems.length; index += 1) {
-    if (index.value == null) {
+    console.log(requiredItems[index]);
+    if (requiredItems[index].value === null || checkedInput === null) {
       validateMessage.innerText = 'Campos inválidos';
+      validate = false;
     }
+  }
+  return validate;
+}
+
+function changeContent(event) {
+  if (validateForm(event)) {
+    const name = document.querySelector('#firstname').value;
+    const lastname = document.querySelector('#lastname').value;
+    const email = document.querySelector('#phone_email').value;
+    const birthdate = document.querySelector('#birthdate-input').value;
+    const gender = document.querySelector('input[type="radio"]:checked').value;
+    const rightContent = document.querySelector('.right-content');
+    rightContent.innerHTML = `<br><br><h2>Olá, ${name} ${lastname}</h2><h3>${email}</h3><h3>${birthdate}</h3><h3>${gender}</h3>`;
   }
 }
 
 const registerBtn = document.getElementById('facebook-register');
-registerBtn.addEventListener('click', validateForm);
+registerBtn.addEventListener('click', changeContent);
