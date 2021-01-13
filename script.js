4ece3ec49dcf1770a80b6c8447e80f5c83955644
João Pedro Barros <joaopedrobarros5@gmail.com>
2021-01-13 15:45:10 -0300

    adicionando alerta corrigido

--- script.js
+++ script.js
@@ -1,27 +1,15 @@
-const getInputsLogin = document.querySelectorAll('.loginInput');
+// let clickEnter = document.getElementById('button-login')
 
-function verifyEmpty(x) {
-  let count = 0;
-  if (x === 0) {
-    count += 1;
-  }
-  return count;
-}
-
-function signalAlert() {
-  alert('Email ou telefone');
-}
-
-function loginEmptyAlert(value) {
-  if (value === 0 || value === 1) {
-    document.getElementById('button-login').addEventListener('click', signalAlert);
-  }
-}
-console.log(loginEmptyAlert(0))
-// function alertMissingInfoLogin() {
-//   let missingInfo = 0;
-//   for (let index = 0; index < getInputsLogin.length; index += 1) {
-//     missingInfo += verifyEmpty(getInputsLogin[index]);
-//   }
-//   return loginEmptyAlert(missingInfo);
+// function loginAlert() {
+//   let inputLogin = document.getElementById('user-email-phone').innerText;
+//   alert(inputLogin)
 // }
+
+// clickEnter.addEventListener('click', loginAlert);
+
+let loginButton = document.getElementById("button-login")
+function alertEmail() {
+    let email = document.getElementById("user-email-phone").value
+    alert(email)
+}
+loginButton.addEventListener('click', alertEmail)
