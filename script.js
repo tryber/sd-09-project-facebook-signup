654f5fc7f8f1c1aa6027a4c5d40ca80243d3bb62
BBrolesi <brunohsbrolesi@gmail.com>
2021-01-13 19:39:37 -0300

    Add custom gender input text

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 function addCustomGenderInput(event) {
   const genderInputContainer = document.getElementById('gender-input-container');
-
-  if (event.checked === true) {
+  if (event.target.value === "Personalizado") {
     const genderInput = document.createElement('input');
     genderInput.type = 'text';
     genderInput.className = 'input';
     genderInput.placeholder = 'Gênero (opcional)';
     genderInput.name = 'gender-custom';
     genderInputContainer.appendChild(genderInput);
-  } else {
-    genderInputContainer.removeChild(genderInputContainer.firstChild);
+  } else if (genderInputContainer.childNodes.length != 0) {
+    while (genderInputContainer.firstChild)
+      genderInputContainer.removeChild(genderInputContainer.lastChild);
   }
+
 }
 
 window.onload = function () {
   const signInButton = document.getElementById('facebook-register');
-  const customGenderOption = document.getElementById('custom-gender-option').checked;
+  const customGenderOptions = document.getElementsByClassName('gender-radio');
 
-  customGenderOption.addEventListener('change', addCustomGenderInput);
-  if (signInButton);
+  for (radio of customGenderOptions) {
+    radio.addEventListener('change', addCustomGenderInput);
+  }
+
 };
