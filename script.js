10622cd5d7cdad8305a82c4dbb3abc033c004876
J.Henrique <jhenriqueribeirodesa@gmail.com>
2021-01-13 17:46:47 -0300

    Requisito 19 - Adiciona input para genero

--- script.js
+++ script.js
@@ -6,6 +6,19 @@ function showEmailPhoneAlert() {
   alert(emailPhone);
 }
 
+function setCustomGenderField () {
+  const customGenderRadio = document.querySelector('#gender-container');
+  const customGenderInput = document.querySelector('#gender-custom');
+  customGenderRadio.addEventListener('change', (event) => {
+    if (event.target.id === 'gender-personalized') {
+      customGenderInput.classList.remove('hide');
+    } else {
+      customGenderInput.classList.add('hide');
+    }
+  });
+}
+
 window.onload = function () {
   buttonLogin.addEventListener('click', showEmailPhoneAlert);
+  setCustomGenderField();
 };
