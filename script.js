5fad9324259d33267e9d38bb6612ae9f6356198f
João Pedro Barros <joaopedrobarros5@gmail.com>
2021-01-13 11:56:12 -0300

    code climate 6

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-const getInputsLogin = document.querySelectorAll(".loginInput");
+const getInputsLogin = document.querySelectorAll('.loginInput');
 
 function verifyEmpty(x) {
   let count = 0;
   if (x === 0) {
-    count = count + 1;
+    count += 1;
   }
   return count;
 }
 
 function signalAlert() {
-  alert("Email ou telefone");
+  alert('Email ou telefone');
 }
 
 function loginEmptyAlert(value) {
   if (value === 0 || value === 1) {
-    document.getElementById('button-login').addEventListener('click', signalAlert)
+    document.getElementById('button-login').addEventListener('click', signalAlert);
   }
 }
-
-function alertMissingInfoLogin() {
-  const missingInfo = 0;
-  for (let index = 0; index < getInputsLogin.length; index += 1) {
-    missingInfo += verifyEmpty(getInputsLogin[index]);
-  }
-  return loginEmptyAlert(missingInfo);
-}
+console.log(loginEmptyAlert(0))
+// function alertMissingInfoLogin() {
+//   let missingInfo = 0;
+//   for (let index = 0; index < getInputsLogin.length; index += 1) {
+//     missingInfo += verifyEmpty(getInputsLogin[index]);
+//   }
+//   return loginEmptyAlert(missingInfo);
+// }
