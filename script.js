89bca3b6370ea2d7c3d5e8415dababbd8869eebf
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2021-01-13 18:46:15 -0300

    atualizando codeClimate

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function alertLogin() {
-    const textInput = document.getElementById('user-email-phone');
-    alert(textInput.value);
+  const textInput = document.getElementById('user-email-phone');
+  alert(textInput.value);
 }
 
 const btLogin = document.getElementById('button-login');
