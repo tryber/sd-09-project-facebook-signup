d40ff0f711ea486ab100ff94eee577d53506033a
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2021-01-13 10:57:53 -0300

    Code climate formating

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
-const button = document.querySelector('#button-login');
-const inputName = document.querySelector('#user-email-phone');
+const button = document.querySelector("#button-login");
+const inputName = document.querySelector("#user-email-phone");
 
-button.addEventListener('click', () => alert(inputName.value));
+button.addEventListener("click", () => alert(inputName.value));
 
-const custom = document.querySelector('#custom-gender');
-const customGender = document.querySelector('#pers');
+const custom = document.querySelector("#custom-gender");
+const customGender = document.querySelector("#pers");
 
-customGender.addEventListener('click', function () {
-  if (customGender.classList.contains('checked')) {
+customGender.addEventListener("click", function () {
+  if (customGender.classList.contains("checked")) {
     return;
   }
-  const newGender = document.createElement('input');
-  newGender.type = 'text';
-  newGender.placeholder = 'Gênero (opcional)';
+  const newGender = document.createElement("input");
+  newGender.type = "text";
+  newGender.placeholder = "Gênero (opcional)";
   newGender.required = true;
   custom.appendChild(newGender);
-  customGender.classList = 'checked';
+  customGender.classList = "checked";
   newGender.required = true;
 });
 
-const genderInput = document.querySelector('#gender-input');
-const notCustom = document.querySelectorAll('.not-custom-gender');
-  notCustom.addEventListener('click', function () {
-    document.getElementById('custom-gender').remove();
-  });
-
+// const genderInput = document.querySelector("#gender-input");
+// const notCustom = document.querySelectorAll(".not-custom-gender");
+// notCustom.addEventListener("click", function () {
+//   document.getElementById("custom-gender").remove();
+// });
