111daba566ca86abc74aa2270b4dddbab8df53ff
Pablo Fernandes <pabloprates@gmail.com>
2021-01-13 14:28:07 -0300

    Adicionando os requisitos 16 e 19

--- script.js
+++ script.js
@@ -4,5 +4,30 @@ function showLoginData(evt) {
   alert(loginEmailPhone);
 }
 
+function createInputField(evt) {
+  let radioColumn = document.getElementById('radios-father');
+  let otherGenderInput = document.getElementById('otherGender');
+  if(evt.target.id === 'other' && otherGenderInput === null) {
+    let newInput = document.createElement('input');
+    newInput.type = 'text';
+    newInput.id = 'otherGender';
+    newInput.placeholder = 'Gênero (opcional)';
+    newInput.name = 'gender-custom';
+    radioColumn.appendChild(newInput);
+  }
+  
+  if(evt.target.id != 'other' && otherGenderInput != null) {
+    radioColumn.removeChild(otherGenderInput);
+  }
+}
+
+function validateInputs() {
+  let inputs = document.getElementsByTagName('input');
+}
+
 const btnEntrar = document.getElementById('button-login');
 btnEntrar.addEventListener('click', showLoginData);
+const GenderRadio = document.getElementById('radios');
+GenderRadio.addEventListener('click', createInputField);
+const btnApply = document.getElementById('facebook-register');
+btnApply.addEventListener('click', validateInputs);
