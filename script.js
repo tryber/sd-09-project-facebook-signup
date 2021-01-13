5fa13552fb3933520fb8330501c80a6be29297fe
Jhonatan Arguello <jhonatan.arguello@gmail.com>
2021-01-13 10:11:09 -0300

    Inserindo mensagem de boas vindas.

--- script.js
+++ script.js
@@ -25,20 +25,37 @@ function checkRegister() {
   let countInvalidFields = 0;
   const errorMessageLocation = document.querySelector('#invalidFields');
   const registerButton = document.querySelector('#facebook-register');
+  let userData = []
 
+  console.log(userData)
   registerButton.addEventListener('click', function (event) {
     for (let index = 0; index < inputLocation.length; index += 1) {
       if (inputLocation[index].value === '') {
         countInvalidFields += 1;
+      } else {
+        userData.push(inputLocation[index].value)
       }
     }
 
+    console.log(userData)
     if (countInvalidFields > 0) {
       event.preventDefault();
       errorMessageLocation.innerText = 'Campos inválidos';
+    } else {
+      const rightContentLocation = document.querySelector('.right-content')
+      while (rightContentLocation.firstChild) {
+        rightContentLocation.removeChild(rightContentLocation.firstChild)
+      }
+
+      let welcomeMessageLocation = document.createElement('p');
+      welcomeMessageLocation.innerText = `Olá ${userData[0]} ${userData[1]}, ${userData[2]} e ${userData[4]}`
+      console.log(userData)
+      rightContentLocation.appendChild(welcomeMessageLocation)
     }
+    console.log(userData)
   });
 }
+
 
 checkRegister();
 genderInput();
