34477d40ae5245c3da4cc5301f30ebbb3ef7769f
peryclesreis <perycles_reis@hotmail.com>
2021-01-12 23:14:06 -0300

    atualizando arquivos para requisitos 02

--- script.js
+++ script.js
@@ -2,16 +2,19 @@ const buttonLogin = document.querySelector('#button-login');
 const roudePersonl = document.querySelector('.radios');
 const inputGenero = document.querySelector('.genero');
 
-buttonLogin.addEventListener('click', function () {
-	const inputEmailPhone = document.querySelector('#user-email-phone').value;
-	alert(inputEmailPhone);
-});
+function alertaBotao() {
+  buttonLogin.addEventListener('click', function () {
+    const inputEmailPhone = document.querySelector('#user-email-phone').value;
+    alert(inputEmailPhone);
+  });
+}
+alertaBotao();
 
 function botaoAbreGenero(event) {
-	if (event.target.value !== 'Personalizado') {
-		inputGenero.style = 'display:none';
-	} else {
-		inputGenero.style = 'display:block';
-	}
+  if (event.target.value !== 'Personalizado') {
+    inputGenero.style = 'display:none';
+  } else {
+    inputGenero.style = 'display:block';
+  }
 }
 roudePersonl.addEventListener('click', botaoAbreGenero);
