f6f0621a4dd801a67218c072d2e0db18c09f3bcf
RodrigoNeoEx <draja.acc@gmail.com>
2021-01-13 15:25:02 -0300

    alterada funçao setvalue e checkvalid

--- script.js
+++ script.js
@@ -10,8 +10,9 @@ function alertLogin() {
 function stopDefault(event) {
   event.preventDefault();
 }
-
+const sectionRight = document.querySelector('#right-content');
 const validateCamps = document.querySelectorAll('.cadastro input');
+const getGender = document.querySelectorAll('.radioButtons input')
 function registerValidate() {
   for (let index = 0; index < 5; index += 1) {
     if (validateCamps[index].value === '') {
@@ -22,54 +23,33 @@ function registerValidate() {
   }
 }
 
-function createValidRegister() {
-  for (let index = 0; index < 5; index += 1) {
-    if (validateCamps[index].value !== '') {
-      const createDiv = document.createElement('div');
-      document.getElementById('appendRegister').appendChild(createDiv);
-    }
-  }
-}
-
-function setValueOnDiv() {
-  const divIndex = document.querySelectorAll('#appendRegister div');
-  divIndex[0].innerHTML = `Olá ${validateCamps[0].value} ${validateCamps[1].value}`;
-  divIndex[1].innerHTML = `${validateCamps[2].name} ${validateCamps[2].value}`;
-  divIndex[2].innerHTML = `${validateCamps[4].name} ${validateCamps[4].value}`;
+function setValueOnSectionRight() {
+  sectionRight.innerHTML = `Olá ${validateCamps[0].value} ${validateCamps[1].value},
+  contato: ${validateCamps[2].value}
+  nascido em: ${validateCamps[4].value}
+  e de ${createGenderRegister()};
+  Seus dados foram cadastrados com sucesso!`;
 }
 
 function createGenderRegister() {
-  const getGender = document.querySelectorAll('.radioButtons input');
+  let gender1 = '';
+  let gender2 = '';
   for (let index = 0; index < 3; index += 1) {
     if (getGender[index].checked === true) {
-      const createDiv = document.createElement('div');
-      createDiv.innerHTML = `${getGender[index].name}: ${getGender[index].value}`;
-      document.getElementById('appendRegister').appendChild(createDiv);
+      gender1 = `genero: ${getGender[index].value}`;
     }
   }
   if (getGender[2].checked === true) {
     const customGender = document.getElementById('genderText');
-    const createDiv = document.createElement('div');
-    createDiv.innerHTML = `${customGender.name}: ${customGender.value}`;
-    document.getElementById('appendRegister').appendChild(createDiv);
+    gender2 = ` > ${customGender.value}`;
   }
-}
-
-function createSection() {
-  const createSectionValidate = document.createElement('section');
-  createSectionValidate.id = 'appendRegister';
-  createSectionValidate.className = 'appendRegister';
-  document.getElementById('right-content').appendChild(createSectionValidate);
+  return gender1 + gender2
 }
 
 function checkValidRegister() {
   const checkDivLenght = document.querySelectorAll('form div');
   if (checkDivLenght.length === 0) {
-    createSection();
-    createValidRegister();
-    setValueOnDiv();
-    createGenderRegister();
-    document.getElementById('cadastro').remove();
+    setValueOnSectionRight();
   }
 }
 
@@ -79,18 +59,6 @@ function listeners() {
   registerButton.addEventListener('click', registerValidate);
   registerButton.addEventListener('click', checkValidRegister);
 }
-
-// function checkEmail() {
-//   const email = document.querySelector('.input-login');
-//   let insertedEmail = email.value;
-//   const emailFormat = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+$/.test(insertedEmail);
-//   if (!emailFormat && insertedEmail.length) {
-//     email.value = '';
-//     alert('email inválido');
-//     return false;
-//   }
-//   return emailFormat
-// }
 
 window.onload = function () {
   alertLogin();
