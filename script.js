98347a96f608381b278c2ade535e5e3f405208ee
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2021-01-13 12:28:19 -0300

    validacao de inputs ao clicar em cadastre

--- script.js
+++ script.js
@@ -1,7 +1,44 @@
 const button = document.querySelector('#button-login');
 const inputName = document.querySelector('#user-email-phone');
-
 button.addEventListener('click', () => alert(inputName.value));
+
+function validateTextInputs() {
+  const inputText = document.querySelectorAll('.right-content input[type=text], input[type=password]');
+  let isTextValid = true;
+  for (let index = 0; index < inputText.length; index += 1) {
+    if (!inputText[index].value) {
+      isTextValid = false;
+    }
+  }
+  return isTextValid;
+}
+
+function validateRadioInput() {
+  const radioInput = document.querySelectorAll('input[type=radio]')
+  let isRadioValid = false;
+  for (let index = 0; index < radioInput.length; index += 1) {
+    if(radioInput[index].checked) {
+      isRadioValid = true;
+    }
+  }
+  return isRadioValid;
+}
+
+function submitButton(event) {
+  event.preventDefault();
+  const invalidField = document.querySelector('#invalid-fields');
+  let isValidText = validateTextInputs();
+  let isValidRadio = validateRadioInput();
+  if (!isValidText || !isValidRadio) {
+    invalidField.innerText = "Campos inválidos";
+  } else {
+    invalidField.innerText = "";
+  }
+}
+
+const submitForm = document.querySelector('#facebook-register');
+submitForm.addEventListener('click', submitButton);
+
 
 const custom = document.querySelector('#custom-gender');
 const customGender = document.querySelector('#unique');
