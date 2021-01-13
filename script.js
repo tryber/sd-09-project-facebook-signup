ce16bf92873a7f52182ac54e7e74717cfb82a41a
Jhonatan Arguello <jhonatan.arguello@gmail.com>
2021-01-13 18:11:30 -0300

    Resoluçao de issues.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function genderInput() {
   const gerderListner = document.querySelector('.gender-control');
 
   gerderListner.addEventListener('click', function () {
-    let radioValue = document.querySelector('input[name="gender"]:checked').value;
+    const radioValue = document.querySelector('input[name="gender"]:checked').value;
 
     if (radioValue === 'Personalizado') {
       document.querySelector('.other-gender').style.display = 'block';
@@ -21,15 +21,15 @@ function logIn() {
 }
 
 function checkDataUser() {
-  const dataUser = document.querySelectorAll('.input')
-  let resultCheckDataUser = 0
+  const dataUser = document.querySelectorAll('.input');
+  let resultCheckDataUser = 0;
 
   for (let index = 0; index < dataUser.length; index += 1) {
     if (dataUser[index].value === '') {
-      resultCheckDataUser += 1
+      resultCheckDataUser += 1;
     }
   }
-  return resultCheckDataUser
+  return resultCheckDataUser;
 }
 
 function removeRightContent() {
@@ -61,87 +61,23 @@ function registerUser() {
   const registerButton = document.querySelector('#facebook-register');
   const dataUser = document.querySelectorAll('.input');
 
-  registerButton.addEventListener('click', function() {
+  registerButton.addEventListener('click', function () {
     if (checkDataUser() > 0) {
       const errorMessageLocation = document.querySelector('#invalidFields');
       errorMessageLocation.innerText = 'Campos inválidos';
     } else {
       const selectedGender = document.querySelector('input[name="gender"]:checked');
-      
+
       removeRightContent();
       createParagraph();
       createParagraph();
       createParagraph();
       createParagraph();
       messageWelcome(dataUser, selectedGender.value);
-    };
-  });
-}
-
-
-/*
-function createParagraph() {
-  const welcomeMessageLocation = document.createElement('p');
-  welcomeMessageLocation.classList.add('welcome');
-
-  const rightContentLocation = document.querySelector('.right-content');
-
-  rightContentLocation.appendChild(welcomeMessageLocation);
-}
-
-function removeRightContent() {
-  const rightContentLocation = document.querySelector('.right-content');
-  while (rightContentLocation.firstChild) {
-    rightContentLocation.removeChild(rightContentLocation.firstChild);
-  }
-}
-
-function messageWelcome(array, gender) {
-  const paragraphWelcome = document.querySelectorAll('.welcome');
-
-  paragraphWelcome[0].innerText = `Olá, ${array[0].value} ${array[1].value}`;
-  paragraphWelcome[1].innerText = `${array[2].value}`;
-  paragraphWelcome[2].innerText = `${array[4].value}`;
-  paragraphWelcome[3].innerText = `${gender}`;
-}
-
-function checkRegister() {
-  const inputLocation = document.querySelectorAll('.input');
-  let countInvalidFields = 0;
-  const errorMessageLocation = document.querySelector('#invalidFields');
-  const registerButton = document.querySelector('#facebook-register');
-  let userGender = document.querySelector('input[name="gender"]:checked');
-  console.log(userGender)
-  registerButton.addEventListener('click', function (event) {
-    userGender = document.querySelector('input[name="gender"]:checked');
-    for (let index = 0; index < inputLocation.length; index += 1) {
-      if (inputLocation[index].value === '') {
-        countInvalidFields += 1;
-      }
-    }
-
-    if (countInvalidFields > 0) {
-      event.preventDefault();
-      errorMessageLocation.innerText = 'Campos inválidos';
-    } else {
-      removeRightContent();
-      createParagraph();
-      createParagraph();
-      createParagraph();
-      createParagraph();
-      messageWelcome(inputLocation);
     }
   });
-  //console.log(userGender)
-
-  
 }
-
-
-checkRegister();
-
-*/
 
 genderInput();
 logIn();
-registerUser()
+registerUser();
