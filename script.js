8f7385be54635f81814aa2a062f7559cdf2851c1
BBrolesi <brunohsbrolesi@gmail.com>
2021-01-12 21:03:41 -0300

    Custom gender input in development

---
+++ script.js
@@ -0,0 +1,22 @@
+window.onload = function () {
+  const signInButton = document.getElementById("facebook-register");
+  const customGenderOption = document.getElementById('custom-gender-option');
+
+  customGenderOption.addEventListener('change', () => alert.customGenderOption);
+}
+
+function addCustomGenderInput(event) {
+  const genderInputContainer = document.getElementById('gender-input-container');
+
+  if (event.checked == true) {
+    const genderInput = document.createElement('input');
+    genderInput.type = 'text';
+    genderInput.className = 'input';
+    genderInput.placeholder = 'Gênero (opcional)';
+    genderInput.name = 'gender-custom';
+    genderInputContainer.appendChild(genderInput);
+  }
+  else{
+      genderInputContainer.removeChild(genderInputContainer.firstChild);
+  }
+}
