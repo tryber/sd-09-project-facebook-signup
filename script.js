e00f1c7940471c0d3bf5342d740995cf067be1bf
MarcioFujita <hanker78@gmail.com>
2021-01-13 12:49:34 -0300

    Requisito 20v9

--- script.js
+++ script.js
@@ -15,6 +15,39 @@ function createP() {
   newP.appendChild(newP2);
 }
 
+function write() {
+  const box = document.getElementById('right-content');
+  const inputName2 = document.getElementById('name');
+  const inputLastname2 = document.getElementById('lastname');
+  const inputPhone2 = document.getElementById('phoneemail');
+  const inputBirth2 = document.getElementById('birthdate');
+  const inputFeminino2 = document.getElementById('feminino');
+  const inputMasculino2 = document.getElementById('masculino');
+  const inputPersonalizado2 = document.getElementById('personalizado');
+  const inputNameValue2 = inputName2.value;
+  const inputLastnameValue2 = inputLastname2.value;
+  const inputPhoneValue2 = inputPhone2.value;
+  const inputBirthValue2 = inputBirth2.value;
+  const inputFemininoValue2 = inputFeminino2.checked;
+  const inputMasculinoValue2 = inputMasculino2.checked;
+  const inputPersonalizadoValue2 = inputPersonalizado2.checked;
+  let checked2 = '';
+  if (inputFemininoValue2 === true) {
+    checked2 = 'Feminino';
+  } else if (inputMasculinoValue2 === true) {
+    checked2 = 'Masculino';
+  } else if (inputPersonalizadoValue2 === true) {
+    checked2 = 'Personalizado';
+  }
+  box.innerText = (`Olá, ${inputNameValue2} ${inputLastnameValue2} ${inputPhoneValue2} ${inputBirthValue2} ${checked2}`);
+}
+
+// function del() {
+//   const box2 = document.getElementById('right-content');
+//   const form2 = document.getElementById('form');
+//   box2.removeChild(form2);
+// }
+
 const btnCadastre = document.getElementById('facebook-register');
 function validInput(event) {
   event.preventDefault();
@@ -41,7 +74,7 @@ function validInput(event) {
       inputPersonalizadoValue === false) {
     createP();
   } else {
-    deleteWrite();
+    write();
   }
 }
 btnCadastre.addEventListener('click', validInput);
@@ -57,32 +90,3 @@ function inputTxtPerso() {
   rdoPerso2.appendChild(rdoPersoNew);
 }
 rdoPerso.addEventListener('click', inputTxtPerso);
-
-function deleteWrite() {
-  const inputName2 = document.getElementById('name');
-  const box = document.getElementById('right-content');
-  const form = document.getElementById('form');
-  const inputLastname2 = document.getElementById('lastname');
-  const inputPhone2 = document.getElementById('phoneemail');
-  const inputBirth2 = document.getElementById('birthdate');
-  const inputFeminino2 = document.getElementById('feminino');
-  const inputMasculino2 = document.getElementById('masculino');
-  const inputPersonalizado2 = document.getElementById('personalizado');
-  const inputNameValue2 = inputName2.value;
-  const inputLastnameValue2 = inputLastname2.value;
-  const inputPhoneValue2 = inputPhone2.value;
-  const inputBirthValue2 = inputBirth2.value;
-  const inputFemininoValue2 = inputFeminino2.checked;
-  const inputMasculinoValue2 = inputMasculino2.checked;
-  const inputPersonalizadoValue2 = inputPersonalizado2.checked;
-  box.removeChild(form);
-  let checked2 = '';
-  if (inputFemininoValue2 === true) {
-    checked2 = 'Feminino';
-  } else if (inputMasculinoValue2 === true) {
-    checked2 = 'Masculino';
-  } else if (inputPersonalizadoValue2 === true) {
-    checked2 = 'Personalizado';
-  }
-  box.innerText = (`Olá, ${inputNameValue2} ${inputLastnameValue2} ${inputPhoneValue2} ${inputBirthValue2} ${checked2}`);
-}
