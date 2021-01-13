2aaea0b0add1b69aed06d42cd48b4541af7bc304
RodrigoNeoEx <draja.acc@gmail.com>
2021-01-13 13:35:28 -0300

    teste function validate

--- script.js
+++ script.js
@@ -22,46 +22,62 @@ function registerValidate() {
   }
 }
 
-// function createValidRegister() {
-//   for (let index = 0; index < 5; index += 1) {
-//     if (validateCamps[index].value !== '') {
-//       const createDiv = document.createElement('div');
-//       document.getElementById('appendRegister').appendChild(createDiv);
-//     }
-//   }
-// }
+function createValidRegister() {
+  for (let index = 0; index < 5; index += 1) {
+    if (validateCamps[index].value !== '') {
+      const createDiv = document.createElement('div');
+      document.getElementById('appendRegister').appendChild(createDiv);
+    }
+  }
+}
 
-// function createGenderRegister() {
-//   const getGender = document.querySelectorAll('.radioButtons input');
-//   for (let index = 0; index < 3; index += 1) {
-//     if (getGender[index].checked === true) {
-//       const createDiv = document.createElement('div');
-//       createDiv.innerHTML = `${getGender[index].name}: ${getGender[index].value}`;
-//       document.getElementById('appendRegister').appendChild(createDiv);
-//     }
-//   }
-//   if (getGender[2].checked === true) {
-//     const customGender = document.getElementById('genderText');
-//     const createDiv = document.createElement('div');
-//     createDiv.innerHTML = `${customGender.name}: ${customGender.value}`;
-//     document.getElementById('appendRegister').appendChild(createDiv);
-//   }
-// }
+function setValueOnDiv() {
+  const divIndex = document.querySelectorAll('#appendRegister div');
+  divIndex[0].innerHTML = `Olá ${validateCamps[0].value} ${validateCamps[1].value}`;
+  divIndex[1].innerHTML = `${validateCamps[2].name} ${validateCamps[2].value}`;
+  divIndex[2].innerHTML = `${validateCamps[4].name} ${validateCamps[4].value}`;
+}
 
-// function checkValidRegister() {
-//   const sectionCheck = document.getElementById('appendRegister').childElementCount;
-//   if (sectionCheck === 0) {
-//     createValidRegister();
-//     createGenderRegister();
-//     document.getElementById('cadastro').remove();
-//   }
-// }
+function createGenderRegister() {
+  const getGender = document.querySelectorAll('.radioButtons input');
+  for (let index = 0; index < 3; index += 1) {
+    if (getGender[index].checked === true) {
+      const createDiv = document.createElement('div');
+      createDiv.innerHTML = `${getGender[index].name}: ${getGender[index].value}`;
+      document.getElementById('appendRegister').appendChild(createDiv);
+    }
+  }
+  if (getGender[2].checked === true) {
+    const customGender = document.getElementById('genderText');
+    const createDiv = document.createElement('div');
+    createDiv.innerHTML = `${customGender.name}: ${customGender.value}`;
+    document.getElementById('appendRegister').appendChild(createDiv);
+  }
+}
+
+function createSection() {
+  const createSection = document.createElement('section');
+  createSection.id = 'appendRegister';
+  createSection.className = 'appendRegister';
+  document.getElementById('right-content').appendChild(createSection);
+}
+
+function checkValidRegister() {
+  const checkDivLenght = document.querySelectorAll('form div');
+  if (checkDivLenght.length === 0) {
+    createSection();
+    createValidRegister();
+    setValueOnDiv();
+    createGenderRegister();
+    document.getElementById('cadastro').remove();
+  }
+}
 
 function listeners() {
   const registerButton = document.getElementById('facebook-register');
   registerButton.addEventListener('click', stopDefault);
   registerButton.addEventListener('click', registerValidate);
-  // registerButton.addEventListener('click', checkValidRegister);
+  registerButton.addEventListener('click', checkValidRegister);
 }
 
 // function checkEmail() {
