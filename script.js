056668196967c84e000bce0bfdc2ca69acdd4dc0
BBrolesi <brunohsbrolesi@gmail.com>
2021-01-13 20:01:05 -0300

    Correct some code climate issues

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
 function addCustomGenderInput(event) {
   const genderInputContainer = document.getElementById('gender-input-container');
-  if (event.target.value === "Personalizado") {
+  if (event.target.value === 'Personalizado') {
     const genderInput = document.createElement('input');
     genderInput.type = 'text';
     genderInput.className = 'input';
     genderInput.placeholder = 'Gênero (opcional)';
     genderInput.name = 'gender-custom';
     genderInputContainer.appendChild(genderInput);
-  } else if (genderInputContainer.childNodes.length != 0) {
-    while (genderInputContainer.firstChild)
-      genderInputContainer.removeChild(genderInputContainer.lastChild);
+  } else if (genderInputContainer.childNodes.length !== 0) {
+    genderInputContainer.removeChild(genderInputContainer.lastChild);
   }
 
 }
 
 window.onload = function () {
-  const signInButton = document.getElementById('facebook-register');
+  // const signInButton = document.getElementById('facebook-register');
   const customGenderOptions = document.getElementsByClassName('gender-radio');
 
-  for (radio of customGenderOptions) {
+  for (const radio of customGenderOptions) {
     radio.addEventListener('change', addCustomGenderInput);
   }
-
 };
