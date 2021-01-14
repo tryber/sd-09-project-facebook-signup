11a8481f9bef6f0a73197bdb274fe55dd2514f0b
Deusivan Gomes <gomesdeusivan@gmail.com>
2021-01-14 18:38:33 -0300

    climate

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const loginButton = document.getElementById('button-login');
-const signupButton = document.getElementById('facebook-register');
-const inputs = document.getElementById('register');
-const input = inputs.getElementsByTagName('input');
+// const signupButton = document.getElementById('facebook-register');
+// const inputs = document.getElementById('register');
+// const input = inputs.getElementsByTagName('input');
 function alertEmail() {
   const email = document.getElementById('user-email-phone').value;
   alert(email);
@@ -10,4 +10,4 @@ function alertEmail() {
 //   for(index = 0; i < )
 // }
 loginButton.addEventListener('click', alertEmail);
-signupButton.addEventListener('click', checkInputs);
+// signupButton.addEventListener('click', checkInputs);
