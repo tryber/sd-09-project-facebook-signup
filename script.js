dc21bbd0b39c4cab78f68c1dfdb299dc4b840d5c
Felipe Pimentel <lipe.pimentel89@gmail.com>
2021-01-14 12:59:59 -0300

    correcoes 1-7 e codeclimate

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-document.getElementById('button-login').addEventListener('click', function() {
-  const user = getElementById('user-email-phone').value
-  alert(user);
+document.getElementById('button-login').addEventListener('click', function () {
+  alert(document.getElementById('user-email-phone').value);
 });
