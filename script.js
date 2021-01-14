905731a499e89e2d942a6b17b3773964d9d1c4c4
Bernardo Smith <bernardostech@gmail.com>
2021-01-14 15:03:00 -0500

    ajustes codeClimate

--- script.js
+++ script.js
@@ -17,31 +17,29 @@ function checkPersonalizado() {
 checkPersonalizado();
 
 function mudarTexto() {
-  let newText = document.querySelector('.main-content');
-  let textName = document.querySelector('.firstname');
-  let textLastName = document.querySelector('.lastname');
-  let textEmailT = document.querySelector('.emailT');
-  let textBirth = document.querySelector('.birth');
+  const newText = document.querySelector('.main-content');
+  const textName = document.querySelector('.firstname');
+  const textLastName = document.querySelector('.lastname');
+  const textEmailT = document.querySelector('.emailT');
+  const textBirth = document.querySelector('.birth');
   newText.innerHTML = `Olá, ${textName.value} ${textLastName.value}
   Email/Telefone: ${textEmailT.value}
   Data de Aniversário: ${textBirth.value}`;
   newText.className = 'right-content';
   newText.style.width = '200px';
-}  
-
-
-function validate (event){
-  let infoForm = document.querySelectorAll('.form-sign-in input');
-  event.preventDefault();
-  let notValid = [];
+}
+function validate(event) {
+  const infoForm = document.querySelectorAll('.form-sign-in input');
+  event.preventDefault ();
+  const notValid = [];
   for (let i = 0; i < infoForm.length; i += 1) {
-    if (infoForm[i].value ==="") {
-      notValid.push([i]);      
+    if (infoForm[i].value === '') {
+      notValid.push([i]);
     }
   }
   if (notValid.length !== 0) {
     document.getElementById('answerVal').innerText = 'Campos inválidos';
-    document.getElementById('answerVal').style.color = 'red'
+    document.getElementById('answerVal').style.color = 'red';
   } else {
     mudarTexto();
   }
