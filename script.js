419162414acde4d2ed95f375229c12dc83f6db6b
johnealves <johne_af@hotmail.com>
2021-01-14 13:06:18 -0300

    add welcome messege

--- script.js
+++ script.js
@@ -3,6 +3,24 @@ button.addEventListener('click', function () {
   alert(document.getElementById('user-email-phone').value);
 });
 
+function welcomeMessege() {
+  const rigthContent = document.getElementById('right-content');
+  const inputName = document.getElementsByName('firstname')[0].value;
+  const inputLastname = document.getElementsByName('lastname')[0].value;
+  const text = document.createElement('p');
+  text.innerHTML = `Olá, ${inputName} ${inputLastname}`;
+  const inputEmail = document.getElementsByName('phone_email')[0].value;
+  const email = document.createElement('p');
+  email.innerHTML = ` e-mail ou telefone: ${inputEmail}`;
+  const birthdate = document.getElementsByName('birthdate')[0].value;
+  const birth = document.createElement('p');
+  birth.innerHTML = `Data de nascimento: ${birthdate}`;
+  rigthContent.innerHTML = '';
+  rigthContent.appendChild(text);
+  rigthContent.appendChild(email);
+  rigthContent.appendChild(birth);
+}
+
 function checkInput() {
   const form = document.getElementById('signup-form');
   const inputName = document.getElementsByName('firstname')[0].value;
@@ -12,6 +30,8 @@ function checkInput() {
   const birthdate = document.getElementsByName('birthdate')[0].value;
   if (inputName === '' || inputLastname === '' || inputEmail === '' || inputPassword === '' || birthdate === '') {
     form.innerHTML = 'Campos inválidos';
+  } else {
+    welcomeMessege()
   }
 }
 
