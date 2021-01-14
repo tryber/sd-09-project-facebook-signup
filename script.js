824472513e7acc36dd89cf4368c33a4b846a7c2a
Cristiano Lopes Duarte <tkdcris@gmail.com>
2021-01-14 11:21:57 -0300

    Mudanças na estrutura script e conclusoes

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const btnEntrar = document.querySelector('#button-login');
+const genre_conteiner = document.querySelector('.genero');
 const choosedGenre = document.getElementById('personalizado');
 
 const alertMsg = (event) => {
@@ -7,28 +8,26 @@ const alertMsg = (event) => {
   event.preventDefault();
 };
 
-document.addEventListener('DOMContentLoaded', function () {
-  const elements = document.getElementsByTagName('input');
-  for (let i = 0; i < elements.length; i += 1) {
-    elements[i].oninvalid = function (e) {
-      e.target.setCustomValidity('');
-      if (!e.target.validity.valid) {
-        document.getElementById('campos_invalidos').innerText = 'Campos inválidos';
-        document.getElementById('campos_invalidos').style.color = 'red';
-      }
-    };
-    elements[i].oninput = function () {
-      document.getElementById('campos_invalidos').innerText = '';
-    };
+const genre = () => {
+  const female = document.getElementById('feminino');
+  const male = document.getElementById('masculino');
+  let textGenre = '';
+  if (female.checked) {
+    textGenre = 'Feminino';
+  } else if (male.checked) {
+    textGenre = 'Masculino';
+  } else {
+    textGenre = 'Personalizado';
   }
-});
+  return textGenre;
+}
 
 const checkGenre = () => {
   const divCustonGenre = document.getElementById('custonGenre');
   while (divCustonGenre.firstChild) {
     divCustonGenre.removeChild(divCustonGenre.lastChild);
   }
-  if (choosedGenre.checked) {
+  if (choosedGenre.id === 'personalizado') {
     const inputCustonGenre = document.createElement('input');
     inputCustonGenre.name = 'gender-custom';
     inputCustonGenre.placeholder = 'Gênero (opcional)';
@@ -40,9 +39,47 @@ const checkGenre = () => {
   }
 };
 
+const clearSection = () => {
+  const sectionToReset = document.querySelector('.dataform');
+  sectionToReset.innerHTML = '';
+};
+
+const validForm = (e) => {
+  const allInputs = document.querySelector('.dataform').querySelectorAll('input');
+  for (let index = 0; index < allInputs.length - 3; index += 1) {
+    if (allInputs[index].value == '') {
+      document.getElementById('campos_invalidos').innerText = 'Campos inválidos';
+      document.getElementById('campos_invalidos').style.color = 'red';
+      e.preventDefault();
+    }
+  }
+}
+
+const onClick = (event) => {
+  validForm();
+  const firstName = document.getElementById('first_name').value;
+  const lastName = document.getElementById('last_name').value;
+  const fullName = `${firstName} ${lastName}`;
+  const telEmail = document.getElementById('telEmail').value;
+  const birthDate = document.getElementById('input_birthdate').value;
+  const textGenre = genre();
+  clearSection();
+  const h1 = document.querySelector('#welcome');
+  h1.innerText = `Olá, ${fullName}`;
+  const p = document.querySelector('.quick-easy');
+    p.innerText = `
+    ${telEmail}
+    ${birthDate}
+    ${textGenre}`;
+  event.preventDefault();
+}
+
+const btnCadastrar = document.getElementById('facebook-register');
+
 const eventslistener = () => {
   btnEntrar.addEventListener('click', alertMsg);
   choosedGenre.addEventListener('click', checkGenre);
+  btnCadastrar.addEventListener('click', onClick)
 };
 
 window.onload = () => {
