2adce17814c64e210ef8ac2b88523162db662b73
Pablo Fernandes <pabloprates@gmail.com>
2021-01-14 15:19:52 -0300

    requisito para validar dados do form

--- script.js
+++ script.js
@@ -22,7 +22,91 @@ function createInputField(evt) {
   }
 }
 
+function removeMessage(messageElement, messageElementFather) {
+  if (messageElement != null) {
+    messageElementFather.removeChild(messageElement);
+  }
+}
+
+function showMessage(messageType, message) {
+  const mainForm = document.querySelector('.main-form');
+  const newMessage = document.createElement('div');
+  const previousErrorMessage = document.getElementById('error-message');
+  removeMessage(previousErrorMessage, mainForm);
+  const previousSuccessMessage = document.getElementById('success-message');
+  removeMessage(previousSuccessMessage, mainForm);
+  newMessage.classList.add('message');
+  newMessage.classList.add(messageType);
+  newMessage.id = `${messageType}-message`;
+  newMessage.innerText = message;
+  mainForm.insertBefore(newMessage, mainForm.firstChild);
+}
+
+function checkInputedData(inputs, index) {
+  switch (true) {
+    case (inputs[index].id === 'first-name' && inputs[index].value === ''):
+      showMessage('error', `The field ${inputs[index].id} must be filled.`);
+      break;
+    case (inputs[index].id === 'last-name' && inputs[index].value === ''):
+      showMessage('error', `The field ${inputs[index].id} must be filled.`);
+      break;
+    case (inputs[index].id === 'user-email-phone-apply' && inputs[index].value === ''):
+      showMessage('error', `The field ${inputs[index].id} must be filled.`);
+      break;
+    case (checkRadioButtons(inputs, index)):
+      showMessage('error', `The field ${inputs[index].name} must have one option checked at least.`);
+      break;
+    case (inputs[index].id === 'password' && inputs[index].value === ''):
+      showMessage('error', `The field ${inputs[index].id} must be filled.`);
+      break;
+    case (inputs[index].id === 'otherGender' && inputs[index].value === ''):
+      showMessage('error', `The field ${inputs[index].id} must be filled.`);
+      break;
+    case (inputs[index].id === 'birthdate' && inputs[index].value === ''):
+      showMessage('error', `The field ${inputs[index].id} must be filled.`);
+      break;
+    default:
+      showMessage('success', 'All ok!');
+  }
+
+  return 1;
+}
+
+function checkRadioButtons(inputs, index) {
+  switch(true) {
+    case (inputs[index].id === 'female' && (inputs[index].checked === false && inputs[index + 1].checked === false && inputs[index + 2].checked === false)):
+      break;
+    case (inputs[index].id === 'male' && (inputs[index].checked === false && inputs[index - 1].checked === false && inputs[index + 1].checked === false)):
+      break;
+    case (inputs[index].id === 'other' && (inputs[index].checked === false && inputs[index - 1].checked === false && inputs[index - 2].checked === false)):
+      break;
+    default:
+      return 0;
+  }
+  return 1;
+}
+
+function showUserData(inputs) {
+  
+}
+
+function validateInputs(evt) {
+  evt.preventDefault();
+  const inputs = document.querySelector('.main-form');
+  inputs.querySelectorAll('.main-form > input');
+  for (let index = 0; index < inputs.length; index += 1) {
+    if (checkInputedData(inputs, index)) {
+      break;
+    }
+  }
+  showUserData(inputs);
+  //Genadi, chame a sua funcao para mostrar os dados aqui!
+
+}
+
 const btnEntrar = document.getElementById('button-login');
 btnEntrar.addEventListener('click', showLoginData);
 const GenderRadio = document.getElementById('radios');
 GenderRadio.addEventListener('click', createInputField);
+const btnApply = document.getElementById('facebook-register');
+btnApply.addEventListener('click', validateInputs);
