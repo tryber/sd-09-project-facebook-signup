8f5aa1b36384c13ea25f45a30fd106d9525b8959
Marina Alane <marina.alane@outlook.com>
2021-01-14 19:23:09 -0300

    ajusta erro

--- script.js
+++ script.js
@@ -8,17 +8,3 @@ function clicar() {
 //  Interpretei errado e a mensagem do alert não era "email ou telefone" mas sim, o valor do//
 //  campo e-mail e telefone. Agora deu!//
 clicar();
-
-function verificarCamposVazios() {
-  const btnforms = document.querySelectorAll('#facebook-register');
-  const forms = document.querySelectorAll('#forms');
-  btnforms.addEventListener('click', function () {
-    for (let i = 0; i < forms.length; i += 1) {
-      if (forms[i].value === '') {
-        alert('Campos Inválidos');
-      }
-    }
-  });
-}
-
-verificarCamposVazios();
