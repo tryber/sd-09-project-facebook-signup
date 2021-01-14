67b97f5e6489cd99340c4685fb8f992b7ad7fefc
Sabrina A <sasa2605.alves@gmail.com>
2021-01-14 10:36:08 -0300

    requirement 19 almost done, left: 18

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ function showAlert() {
   alert(document.getElementById('user-email-phone').value);
 }
 btnAlert.addEventListener('click', showAlert);
+
+const otherGender = document.getElementById('other');
+
+function personalizedGender () {
+  const father = document.getElementById('inputParent')
+  const child = document.createElement('input')
+  father.appendChild(child)
+  child.setAttribute('name', 'gender-custom');
+  child.setAttribute('placeholder', 'Gênero(opcional)')
+}
+
+otherGender.addEventListener('click',personalizedGender );
