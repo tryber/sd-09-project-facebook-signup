c21e1a1ba481e530db5d42081feea71ede744139
Genadi Reuben <genadireuben91@gmail.com>
2021-01-14 15:20:34 -0300

    fazendo pull

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ function showLoginData(evt) {
 
 const btnEntrar = document.getElementById('button-login');
 btnEntrar.addEventListener('click', showLoginData);
+
+
+const registerButton = document.getElementById('facebook-register');
+
+registerButton.addEventListener('click', () => {
+  const rightContent = document.querySelector('.right-content');
+  alert('clickou')
+})
