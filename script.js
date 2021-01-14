446f5604b5e9e98f37212c5de3027edc559584b3
Pedro Castro <pa.coelho.castro@gmail.com>
2021-01-13 23:46:19 -0300

    adicionando requisito 19 + aplicacao de arrow functions

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-function alertLogin() {
-  const textInput = document.getElementById('user-email-phone');
-  alert(textInput.value);
-}
+const genderCustomInput = document.querySelector('.gender-custom')
 
 const btLogin = document.getElementById('button-login');
-btLogin.addEventListener('click', alertLogin);
+btLogin.addEventListener('click', () => {
+  const textInput = document.getElementById('user-email-phone');
+  alert(textInput.value);
+});
+
+const genderSelector = document.querySelector('.radio-gender');
+genderSelector.addEventListener('click', (e) => {
+  const genderSelected = e.target
+  if (genderSelected.value === 'Personalizado') {
+    genderCustomInput.style.display = 'inline'
+  } else {
+    genderCustomInput.style.display = 'none'
+  }
+})
