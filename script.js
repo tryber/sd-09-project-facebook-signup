e1c492c429aa6580e4c758d971d98516a1808a0f
jorgeramires <jorgerl123@gmail.com>
2021-01-14 10:44:01 -0300

    resolvendo codeclimate

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-//const nameEmail = document.getElementById('user-email-phone-label');
-//const passWord = document.getElementById('user-password');
 const buttonLogin = document.getElementById('button-login');
 
 function buttonLoginClick(event) {
   buttonLogin.addEventListener('click', () => {
-     });
+  });
 
-  console.alert(event)
+  console.alert(event);
 };
 buttonLoginClick();
