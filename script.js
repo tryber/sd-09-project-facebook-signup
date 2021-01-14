2e767da91355584013ef0fcf7654354f1c76f0cb
Pablo Fernandes <pabloprates@gmail.com>
2021-01-14 14:46:45 -0300

    CC war round 1

--- script.js
+++ script.js
@@ -5,19 +5,25 @@ function showLoginData(evt) {
 }
 
 function createInputField(evt) {
-  let radioColumn = document.getElementById('radios-father');
-  let otherGenderInput = document.getElementById('otherGender');
-  if(evt.target.id === 'other' && otherGenderInput === null) {
-    let newInput = document.createElement('input');
+  const radioColumn = document.getElementById('radios-father');
+  const otherGenderInput = document.getElementById('otherGender');
+  if (evt.target.id === 'other' && otherGenderInput === null) {
+    const newInput = document.createElement('input');
     newInput.type = 'text';
     newInput.id = 'otherGender';
     newInput.placeholder = 'Gênero (opcional)';
     newInput.name = 'gender-custom';
     radioColumn.appendChild(newInput);
   }
-  
-  if(evt.target.id != 'other' && otherGenderInput != null) {
+
+  if (evt.target.id !== 'other' && otherGenderInput !== null) {
     radioColumn.removeChild(otherGenderInput);
+  }
+}
+
+function removeMessage(messageElement, messageElementFather) {
+  if (messageElement != null) {
+    messageElementFather.removeChild(messageElement);
   }
 }
 
@@ -35,54 +41,51 @@ function showMessage(messageType, message) {
   mainForm.insertBefore(newMessage, mainForm.firstChild);
 }
 
-function removeMessage(messageElement, messageElementFather) {
-  if(messageElement != null){
-    messageElementFather.removeChild(messageElement);
+function checkInputedData(inputs, index) {
+  switch (true) {
+    case (inputs[index].id === 'first-name' && inputs[index].value === ''):
+      showMessage('error', `The field ${inputs[index].id} must be filled.`);
+      break;
+    case (inputs[index].id === 'last-name' && inputs[index].value === ''):
+      showMessage('error', `The field ${inputs[index].id} must be filled.`);
+      break;
+    case (inputs[index].id === 'user-email-phone-apply' && inputs[index].value === ''):
+      showMessage('error', `The field ${inputs[index].id} must be filled.`);
+      break;
+    case (inputs[index].id === 'password' && inputs[index].value === ''):
+      showMessage('error', `The field ${inputs[index].id} must be filled.`);
+      break;
+    case (inputs[index].id === 'female' && (inputs[index].checked === false && inputs[index + 1].checked === false && inputs[index + 2].checked === false)):
+      showMessage('error', `The field ${inputs[index].name} must have one option checked at least.`);
+      break;
+    case (inputs[index].id === 'male' && (inputs[index].checked === false && inputs[index - 1].checked === false && inputs[index + 1].checked === false)):
+      showMessage('error', `The field ${inputs[index].name} must have one option checked at least.`);
+      break;
+    case (inputs[index].id === 'other' && (inputs[index].checked === false && inputs[index - 1].checked === false && inputs[index - 2].checked === false)):
+      showMessage('error', `The field ${inputs[index].name} must have one option checked at least.`);
+      break;
+    case (inputs[index].id === 'otherGender' && inputs[index].value === ''):
+      showMessage('error', `The field ${inputs[index].id} must be filled.`);
+      break;
+    case (inputs[index].id === 'birthdate' && inputs[index].value === ''):
+      showMessage('error', `The field ${inputs[index].id} must be filled.`);
+      break;
+    default:
+      showMessage('success', 'All ok!');
   }
+
+  return 1;
 }
 
 function validateInputs(evt) {
   evt.preventDefault();
   const inputs = document.querySelector('.main-form');
   inputs.querySelectorAll('.main-form > input');
-  const condition = true;
   for (let index = 0; index < inputs.length; index += 1) {
-    switch (condition) {
-      case (inputs[index].id === 'first-name' && inputs[index].value === ''):
-        showMessage('error', `The field ${inputs[index].id} must be filled.`);
-        return 1;
-      case (inputs[index].id === 'last-name' && inputs[index].value === ''):
-        showMessage('error', `The field ${inputs[index].id} must be filled.`);
-        return 1;
-      case (inputs[index].id === 'user-email-phone-apply' && inputs[index].value === ''):
-        showMessage('error', `The field ${inputs[index].id} must be filled.`);
-        return 1;
-      case (inputs[index].id === 'password' && inputs[index].value === ''):
-        showMessage('error', `The field ${inputs[index].id} must be filled.`);
-        return 1;
-      case (inputs[index].id === 'female' && (inputs[index].checked === false && inputs[index + 1].checked === false && inputs[index + 2].checked === false)):
-        showMessage('error', `The field ${inputs[index].name} must have one option checked at least.`);
-        return 1;
-      case (inputs[index].id === 'male' && (inputs[index].checked === false && inputs[index - 1].checked === false && inputs[index + 1].checked === false)):
-        showMessage('error', `The field ${inputs[index].name} must have one option checked at least.`);
-        return 1;
-      case (inputs[index].id === 'other' && (inputs[index].checked === false && inputs[index - 1].checked === false && inputs[index - 2].checked === false)):
-        showMessage('error', `The field ${inputs[index].name} must have one option checked at least.`);
-        return 1;
-      case (inputs[index].id === 'otherGender' && inputs[index].value === ''):
-        showMessage('error', `The field ${inputs[index].id} must be filled.`);
-        return 1;
-      case (inputs[index].id === 'birthdate' && inputs[index].value === ''):
-        showMessage('error', `The field ${inputs[index].id} must be filled.`);
-        return 1;
-      default:
-        break;
+    if (checkInputedData(inputs, index)) {
+      break;
     }
   }
-
-  showMessage('success', 'All ok!');
-  return 0;
-
 }
 
 const btnEntrar = document.getElementById('button-login');
