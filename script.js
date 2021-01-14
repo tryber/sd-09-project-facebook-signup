22b4fc1fa7ee706bd38f08b056eed04577610f56
johnealves <johne_af@hotmail.com>
2021-01-14 11:55:53 -0300

    add check input

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 const button = document.getElementById('button-login');
-
 button.addEventListener('click', function () {
   alert(document.getElementById('user-email-phone').value);
 });
+
+document.getElementById('facebook-register').addEventListener('click', checkInput)
+
+
+function checkInput() {
+  const form = document.getElementById('signup-form')
+  const inputName = document.getElementsByName('firstname')[0].value;
+  const inputLastname = document.getElementsByName('lastname')[0].value;
+  const inputEmail = document.getElementsByName('phone_email')[0].value;
+  const inputPassword = document.getElementsByName('password')[0].value;
+  const birthdate = document.getElementsByName('birthdate')[0].value;
+  if (inputName === '' || inputLastname === '' || inputEmail === '' || inputPassword === '' || birthdate === '') {
+    form.innerHTML = 'Campos inválidos';
+  }
+}
