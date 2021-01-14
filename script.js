ea2f048c25ccf33b5c43a9535aa25f0ed855d27d
Deusivan Gomes <gomesdeusivan@gmail.com>
2021-01-14 12:43:23 -0300

    requisito 2 incompleto

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-// let clickEnter = document.getElementById('button-login')
-
-// function loginAlert() {
-//   let inputLogin = document.getElementById('user-email-phone').innerText;
-//   alert(inputLogin)
-// }
-
-// clickEnter.addEventListener('click', loginAlert);
-
 const loginButton = document.getElementById('button-login');
 function alertEmail() {
   const email = document.getElementById('user-email-phone').value;
