def0a54a80b88e1f90fee9d16d231ab243499004
Bernardo Smith <bernardostech@gmail.com>
2021-01-14 14:50:18 -0500

    tentando acertar a validation #2

--- script.js
+++ script.js
@@ -16,23 +16,36 @@ function checkPersonalizado() {
 }
 checkPersonalizado();
 
+function mudarTexto() {
+  let newText = document.querySelector('.main-content');
+  let textName = document.querySelector('.firstname');
+  let textLastName = document.querySelector('.lastname');
+  let textEmailT = document.querySelector('.emailT');
+  let textBirth = document.querySelector('.birth');
+  newText.innerHTML = `Olá, ${textName.value} ${textLastName.value}
+  Email/Telefone: ${textEmailT.value}
+  Data de Aniversário: ${textBirth.value}`;
+  newText.className = 'right-content';
+  newText.style.width = '200px';
+}  
+
+
 function validate (event){
   let infoForm = document.querySelectorAll('.form-sign-in input');
   event.preventDefault();
   let notValid = [];
-  for (let i = 0; i <= infoForm.length; i += 1) {
+  for (let i = 0; i < infoForm.length; i += 1) {
     if (infoForm[i].value ==="") {
-      notValid.push([i]);
-
-      
+      notValid.push([i]);      
     }
   }
-  if (notValid.length !== "") {
+  if (notValid.length !== 0) {
     document.getElementById('answerVal').innerText = 'Campos inválidos';
     document.getElementById('answerVal').style.color = 'red'
   } else {
-    
+    mudarTexto();
   }
 }
+
 const validation = document.getElementById('facebook-register');
 validation.addEventListener('click', validate);
