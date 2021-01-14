8fad63b91010706c5a99fa4f9818e42f5852cbd8
Fabio Venancio Almeida <fvalmeida.@gmail.com>
2021-01-14 13:22:04 -0300

    novo campo

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-const nameEmail = document.getElementById("user-email-phone-label");
-const passWord = document.getElementById("user-password");
 const buttonLogin = document.getElementById("button-login");
+const nameLogin = document.getElementById('user-email-phone-label');
+const passWordlogin = document.getElementById('user-password');
 
+// function click
 
 function buttonLoginClick(event) {
   buttonLogin.addEventListener("click", (event) => {
     const evento = event.target.type;
     console.log(evento);
   })
-
-  console.alert(event)
 };
 buttonLoginClick()
