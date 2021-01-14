aa93e6b5340c4c898a42a7387865ac47351e3516
Deusivan Gomes <gomesdeusivan@gmail.com>
2021-01-14 18:36:52 -0300

    teste

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const loginButton = document.getElementById('button-login');
-// const signupButton = document.getElementById('facebook-register')
-// const inputs = document.get
+const signupButton = document.getElementById('facebook-register');
+const inputs = document.getElementById('register');
+const input = inputs.getElementsByTagName('input');
 function alertEmail() {
   const email = document.getElementById('user-email-phone').value;
   alert(email);
 }
 // function checkInputs() {
+//   for(index = 0; i < )
 // }
 loginButton.addEventListener('click', alertEmail);
+signupButton.addEventListener('click', checkInputs);
