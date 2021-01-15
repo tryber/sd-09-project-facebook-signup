647ff02d6b33710d0d1d100ef78cf527946ad074
Rafael Pufal <rafael.pufal23@gmail.com>
2021-01-14 21:04:46 -0300

    adicionando requisito 18

--- script.js
+++ script.js
@@ -5,3 +5,32 @@ function alertButton() {
 }
 const botao = document.getElementById('button-login');
 botao.addEventListener('click', alertButton);
+
+
+const showPersonalyzed = () => {
+  let divPlaceholderPersonalyzed = document.getElementById("personalyzedPlaceholder");
+  let childrenDiv = divPlaceholderPersonalyzed.childNodes;
+  if (childrenDiv.length > 0){
+  } else {
+  let inputPersonalyzed = document.createElement("INPUT");
+  divPlaceholderPersonalyzed.appendChild(inputPersonalyzed);
+  inputPersonalyzed.setAttribute('placeholder', 'Gênero (opcional)');
+  inputPersonalyzed.setAttribute('name', 'gender-custom');
+  }  
+}
+
+const removePersonalyzed = () => {
+  let divPlaceholderPersonalyzed = document.getElementById("personalyzedPlaceholder");
+  let childrenDiv = divPlaceholderPersonalyzed.childNodes;
+  if (childrenDiv.length > 0){
+    divPlaceholderPersonalyzed.removeChild(divPlaceholderPersonalyzed.firstChild);
+  } else {
+  }
+}
+
+const personalizedGender = document.getElementById("personalyzed-gender");
+personalizedGender.addEventListener('click', showPersonalyzed);
+
+const otherGender = document.querySelectorAll(".other-gender");
+otherGender[0].addEventListener("click", removePersonalyzed);
+otherGender[1].addEventListener("click", removePersonalyzed);
