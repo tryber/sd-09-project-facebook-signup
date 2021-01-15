7900c7ad19c7f73be08609d6225445998d855841
jlfagundes <jlfagundes@gmail.com>
2021-01-15 08:41:43 -0300

    Requisito 18 inserido para avaliaçao

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ document.querySelector('#button-login').addEventListener('click', function () {
   const userEmailPhoneValue = document.querySelector('#user-email-phone').value;
   alert(userEmailPhoneValue);
 });
+
+
+function Validate_form() {
+  var inputs = document.getElementsByClassName('required');
+  var lengths = inputs.length;
+  var valid = true;
+  for(var i=0; i < lengths; i++){
+    if (!inputs[i].value){ valid = false; }
+  }
+  if (!valid){
+  alert('Campos Inválidos');
+  return false;
+  } else { return true; }
+  }
