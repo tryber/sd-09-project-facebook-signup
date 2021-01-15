d34fdd56dc81843fec09634127f6f62248e97fca
Danrlley Rodrigues <danrlley.c.a.rodrigues@gmail.com>
2021-01-15 03:05:41 -0300

    Add Requisito 19

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 document.querySelector('#button-login').addEventListener('click', function () {
   alert(document.getElementById('user-email-phone').value);
 });
+
+function displayCustomGender() {
+  if (document.querySelector('#select-gender-other').checked === true) {
+    let inputGender = document.createElement('input');
+    inputGender.name = 'gender-custom';
+    inputGender.placeholder = 'Gênero';
+    document.querySelector('.form-data').lastChild.previousElementSibling.previousElementSibling.appendChild(inputGender);
+  } 
+}
+
+displayCustomGender();
