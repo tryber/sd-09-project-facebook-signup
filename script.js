0934b92fb46db3efa4c1c2f70623c4ec4602beba
Carine Becker <caarine.m@gmail.com>
2021-01-15 10:30:57 -0300

    fixed code climate issues

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ document.querySelector('#button-login').addEventListener('click', function () {
   alert(userEmailPhoneValue);
 });
 
-function validateForm() {
-  let inputs = document.getElementsByClassName('required');
-  let lengths = inputs.length;
-  let valid = true;
-  for (let i = 0; i < lengths; i += 1){
-    if (inputs[i].value) {
-      valid = false; }
-  }
-  if (valid) {
-    return true;
-  }
-    alert('Campos Inválidos');
-}
+// function validateForm() {
+//   let inputs = document.getElementsByClassName('required');
+//   let lengths = inputs.length;
+//   let valid = true;
+//   for (let i = 0; i < lengths; i += 1){
+//     if (inputs[i].value) {
+//       valid = false; }
+//   }
+//   if (valid) {
+//     return true;
+//   }
+//     alert('Campos Inválidos');
+// }
 
-document.querySelector('#facebook-register').addEventListener('click', validateForm);
+// document.querySelector('#facebook-register').addEventListener('click', validateForm);
