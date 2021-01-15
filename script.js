29cb22e6e900704ed0923db2fbfca3e10daded18
cleber-teixeira <cleberlt@gmail.com>
2021-01-14 22:24:27 -0300

    Requisito 18 - validar campos. Tentativa 2

--- script.js
+++ script.js
@@ -12,8 +12,6 @@ function validation() {
   const phoneEmail = document.querySelector('#phone-email');
   const newPassword = document.querySelector('#pass-word');
   const birthDate = document.querySelector('#birthdate');
-  // const gender = document.querySelector('input[name="gender"]:checked');
-  const gender = document.getElementsByName('gender');
   const button = document.querySelector('#facebook-register');
   button.addEventListener('click', function () {
     if (firstName.value === '') {
@@ -27,13 +25,22 @@ function validation() {
     } else if (birthDate.value === '') {
       alert('Campos inválidos');
     } else {
-      for (let index = 0; index < gender.length; index += 1) {
-        if (gender[index].checked !== true) {
-          alert('Campos inválidos');
-        }
-      }
+      validationRadio();
     }
   });
+}
+
+function validationRadio() {
+  const gender = document.getElementsByName('gender');
+  let aux = 0;
+  for (let index = 0; index < gender.length; index += 1) {
+    if (gender[index].checked !== true) {
+      aux += 1;
+    }
+  }
+  if (aux === 3) {
+    alert('Campos inválidos');
+  }
 }
 
 window.onload = function () {
