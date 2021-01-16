e45de73731624c1509d39ecd842a59a4a62d46a7
Lucas Henrique Brito <lucas.silva.brito@hotmail.com.br>
2021-01-15 21:56:47 -0300

    Requisito 18

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const btnEntrar = document.querySelector('#button-login');
+const btnEnter = document.querySelector('#button-login');
 
 function addAlert() {
-  btnEntrar.addEventListener('click', function () {
+  btnEnter.addEventListener('click', function () {
     const valueEmail = document.querySelector('#user-email-phone');
     alert(valueEmail.value);
   });
@@ -9,8 +9,29 @@ function addAlert() {
 
 addAlert();
 
-const optCustom = document.querySelector('#Personalizado');
 const listRegister = document.querySelector('.register');
+const btnRegister = document.querySelector('#facebook-register');
+const requiredCamps = document.getElementsByClassName('confirmation');
+
+function checkCamps() {
+  let emptyCamps = 0;
+  btnRegister.addEventListener('click', function () {
+    for (let i = 0; i < requiredCamps.length; i += 1) {
+      if (requiredCamps[i].innerText === '') {
+        emptyCamps += 1; 
+      }
+    }
+    if (emptyCamps > 0) {
+      const requiringMessage = document.createElement('p');
+      requiringMessage.innerText = 'Campos inválidos';
+      listRegister.appendChild(requiringMessage);
+    }
+  });
+}
+
+checkCamps();
+
+const optCustom = document.querySelector('#Personalizado');
 
 function addTextBoxCustom() {
   optCustom.addEventListener('click', function () {
@@ -18,6 +39,7 @@ function addTextBoxCustom() {
       const textBoxCustom = document.createElement('input');
       textBoxCustom.type = 'text';
       textBoxCustom.name = 'gender-custom';
+      textBoxCustom.className = 'confirmation';
       textBoxCustom.placeholder = 'Gênero (opcional)';
       listRegister.insertBefore(textBoxCustom, listRegister.lastElementChild);
     }
