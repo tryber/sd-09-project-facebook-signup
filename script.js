907575cc7fbfe4154da0bacc489690ca4ba743b4
cleber-teixeira <cleberlt@gmail.com>
2021-01-16 20:37:05 -0300

    Refatoração requisito 20.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* Ao clicar no botão Entrar exibe um "alert"
+com o valor do campo "Email ou telefone" */
 function login() {
   const btn = document.querySelector('#button-login');
   btn.addEventListener('click', function (event) {
@@ -9,12 +11,14 @@ function login() {
   });
 }
 
+// Adiciona a mensagem "Campos inválidos"
 function invalidFields() {
   const span = document.getElementById('invalid-fields');
   span.className = 'invalid-fields';
   span.innerText = 'Campos inválidos';
 }
 
+// Realiza a validação dos botões de radio
 function validationRadio() {
   const gender = document.getElementsByName('gender');
   let result;
@@ -33,6 +37,8 @@ function validationRadio() {
   return result;
 }
 
+/* Adiciona o campo "gênero personalizado"
+ao clicar no botão de rádio "Personalizado" */
 function genderCustom() {
   const option = document.querySelector('input[name="gender"]:checked').value;
   const element = document.querySelector('#gender-custom');
@@ -48,16 +54,125 @@ function genderCustom() {
   }
 }
 
+// Capitura o "click" no botão de radio "Personalizado"
 function clickRadio() {
   const optionCustom = document.querySelector('#custom');
   optionCustom.addEventListener('click', genderCustom);
 }
 
+
+
+// Cria o container "right-content"
+function confirmRegistration() {
+  const mainContent = document.querySelector('main');
+  const rightContent = document.createElement('div');
+  rightContent.className = 'right-content teste resume';
+  mainContent.appendChild(rightContent);
+  addData();
+}
+
+// Adiciona os campos "first-name" e "last-name"
+function fullName() {
+  const fName = document.querySelector('#first-name').value;
+  const lName = document.querySelector('#last-name').value;
+  const name = document.createElement('p');
+  name.innerHTML = `✌😎 Olá, <strong>${fName} ${lName}</strong>! Beleza?!`;
+  return name;
+}
+
+function addPhoneOrEmail() {
+  const phoneOrEmail = document.querySelector('#phone-email').value;
+  const emailPhone = document.createElement('p');
+  emailPhone.innerHTML = `‣ Telefone / e-mail: <strong>${phoneOrEmail}</strong>`;
+  return emailPhone;
+}
+
+function addBirthDate() {
+  const birthday = document.querySelector('#birthdate').value;
+  const date = document.createElement('p');
+  date.innerHTML = `‣ Data de Nasc.: <strong>${birthday}</strong>`;
+  return date;
+}
+
+function addGender() {
+  const genderOption = document.querySelector('input[name="gender"]:checked').value;
+  const optionGender = document.createElement('p');
+  optionGender.innerHTML = `‣ Gênero: <strong>${genderOption}</strong>`;
+  return optionGender;
+}
+
+function addMensage() {
+  const mensage = document.createElement('p');
+  mensage.innerHTML = `Projeto realizado por:</br>
+   <p>👉 <em>Frederico Canoeira</em></p>
+   <p>👉 <em>Cleber Teixeira</em></p>
+   <p><strong>#GoTrybe!</strong> 🚀</p>`;
+  return mensage;
+}
+
+// Adiciona dados cadastrados
+function addData() {
+  const rightContainer = document.querySelector('.teste');
+  rightContainer.appendChild(fullName());
+  rightContainer.appendChild(addPhoneOrEmail());
+  rightContainer.appendChild(addBirthDate());
+  rightContainer.appendChild(addGender());
+  rightContainer.appendChild(addMensage());
+}
+
+// function confirmRegistration() {
+//   const mainContent = document.querySelector('main');
+//   const rightContent = document.createElement('div');
+//   const resume = document.createElement('div');
+//   const fName = document.querySelector('#first-name').value;
+//   const lName = document.querySelector('#last-name').value;
+//   const birthday = document.querySelector('#birthdate').value;
+//   const phoneOrEmail = document.querySelector('#phone-email').value;
+//   const genderOption = document.querySelector('input[name="gender"]:checked').value;
+//   rightContent.className = 'right-content resume';
+//   resume.innerHTML = `<p>✌😎 Olá, <strong>${fName} ${lName}</strong>! Beleza?!</p>
+//   <p>‣ Telefone / e-mail: <strong>${phoneOrEmail}</strong></p>
+//   <p>‣ Data de Nasc.: <strong>${birthday}</strong></p>
+//   <p>‣ Gênero: <strong>${genderOption}</strong></p>
+//   <br><p>Projeto realizado por:</p>
+//   <p>👉 <em>Frederico Canoeira</em></p>
+//   <p>👉 <em>Cleber Teixeira</em></p>
+//   <p><strong>#GoTrybe!</strong> 🚀</p>`;
+//   rightContent.appendChild(resume);
+//   mainContent.appendChild(rightContent);
+// }
+
+// Limpa o "right-content"
 function clearForm() {
   const form = document.querySelector('.right-content');
   form.style.display = 'none';
+  confirmRegistration();
 }
 
+// Realiza a validação dos dados de cadastro antes do envio
+function confirmValidation() {
+  const firstName = document.querySelector('#first-name').value;
+  const lastName = document.querySelector('#last-name').value;
+  const phoneEmail = document.querySelector('#phone-email').value;
+  const newPassword = document.querySelector('#pass-word').value;
+  const birthDate = document.querySelector('#birthdate').value;
+  if (firstName !== '') {
+    if (lastName !== '') {
+      if (phoneEmail !== '') {
+        if (newPassword !== '') {
+          if (birthDate !== '') {
+            if (validationRadio() === true) {
+              clearForm();
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+/* Realiza a validação de cada campo e verifica se foi preenchido
+caso contrário chama a função que exibe a mensagem "Campos inválidos" */
 function validation() {
   const button = document.querySelector('#facebook-register');
   button.addEventListener('click', function (event) {
@@ -82,50 +197,6 @@ function validation() {
     }
     confirmValidation();
   });
-}
-
-function confirmRegistration() {
-  const mainContent = document.querySelector('main');
-  const rightContent = document.createElement('div');
-  rightContent.className = 'right-content resume';
-  const resume = document.createElement('div');
-  const fName = document.querySelector('#first-name').value;
-  const lName = document.querySelector('#last-name').value;
-  const birthday = document.querySelector('#birthdate').value;
-  const phoneOrEmail = document.querySelector('#phone-email').value;
-  const genderOption = document.querySelector('input[name="gender"]:checked').value;
-  resume.innerHTML = `<p>✌😎 Olá, <strong>${fName} ${lName}</strong>! Beleza?!</p>
-  <p>‣ Telefone / e-mail: <strong>${phoneOrEmail}</strong></p>
-  <p>‣ Data de Nasc.: <strong>${birthday}</strong></p>
-  <p>‣ Gênero: <strong>${genderOption}</strong></p>
-  <br><p>Projeto realizado por:</p>
-  <p>👉 <em>Frederico Canoeira</em></p>
-  <p>👉 <em>Cleber Teixeira</em></p>
-  <p><strong>#GoTrybe!</strong> 🚀</p>`;
-  rightContent.appendChild(resume);
-  mainContent.appendChild(rightContent);
-}
-
-function confirmValidation() {
-  const firstName = document.querySelector('#first-name').value;
-  const lastName = document.querySelector('#last-name').value;
-  const phoneEmail = document.querySelector('#phone-email').value;
-  const newPassword = document.querySelector('#pass-word').value;
-  const birthDate = document.querySelector('#birthdate').value;
-  if (firstName !== '') {
-    if (lastName !== '') {
-      if (phoneEmail !== '') {
-        if (newPassword !== '') {
-          if (birthDate !== '') {
-            if (validationRadio() === true) {
-              clearForm();
-              confirmRegistration();
-            }
-          }
-        }
-      }
-    }
-  }
 }
 
 window.onload = function () {
