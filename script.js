103adee019b6ed89512f62e21b75b25a30f3e8d5
cleber-teixeira <cleberlt@gmail.com>
2021-01-16 03:13:55 -0300

    Requisito 20 - Substituir o conteúdo do container com a classe "right-content" se o formulário estiver validado."

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ function invalidFields() {
 
 function validationRadio() {
   const gender = document.getElementsByName('gender');
+  let result;
   let aux = 0;
   for (let index = 0; index < gender.length; index += 1) {
     if (gender[index].checked !== true) {
@@ -25,7 +26,11 @@ function validationRadio() {
   }
   if (aux === 3) {
     invalidFields();
+    result = false;
+  } else {
+    result = true;
   }
+  return result;
 }
 
 function genderCustom() {
@@ -48,32 +53,79 @@ function clickRadio() {
   optionCustom.addEventListener('click', genderCustom);
 }
 
+function clearForm() {
+  const form = document.querySelector('.right-content');
+  form.style.display = 'none';
+}
+
 function validation() {
   const button = document.querySelector('#facebook-register');
   button.addEventListener('click', function (event) {
     event.preventDefault();
-    const span = document.getElementById('invalid-fields');
-    const firstName = document.querySelector('#first-name');
-    const lastName = document.querySelector('#last-name');
-    const phoneEmail = document.querySelector('#phone-email');
-    const newPassword = document.querySelector('#pass-word');
-    const birthDate = document.querySelector('#birthdate');
-    span.className = '';
-    span.innerText = '';
-    if (firstName.value === '') {
+    const firstName = document.querySelector('#first-name').value;
+    const lastName = document.querySelector('#last-name').value;
+    const phoneEmail = document.querySelector('#phone-email').value;
+    const newPassword = document.querySelector('#pass-word').value;
+    const birthDate = document.querySelector('#birthdate').value;
+    if (firstName === '') {
       invalidFields();
-    } else if (lastName.value === '') {
+    } else if (lastName === '') {
       invalidFields();
-    } else if (phoneEmail.value === '') {
+    } else if (phoneEmail === '') {
       invalidFields();
-    } else if (newPassword.value === '') {
+    } else if (newPassword === '') {
       invalidFields();
-    } else if (birthDate.value === '') {
+    } else if (birthDate === '') {
       invalidFields();
     } else {
       validationRadio();
     }
+    confirmValidation();
   });
+}
+
+function confirmRegistration() {
+  const mainContent = document.querySelector('main');
+  const rightContent = document.createElement('div');
+  rightContent.className = 'right-content resume';
+  const resume = document.createElement('div');
+  const fName = document.querySelector('#first-name').value;
+  const lName = document.querySelector('#last-name').value;
+  const birthday = document.querySelector('#birthdate').value;
+  const phoneOrEmail = document.querySelector('#phone-email').value;
+  const genderOption = document.querySelector('input[name="gender"]:checked').value;
+  resume.innerHTML = `<p>✌😎 Olá, <strong>${fName} ${lName}</strong>! Beleza?!</p>
+  <p>‣ Telefone / e-mail: <strong>${phoneOrEmail}</strong></p>
+  <p>‣ Data de Nasc.: <strong>${birthday}</strong></p>
+  <p>‣ Gênero: <strong>${genderOption}</strong></p>
+  <br><p>Projeto realizado por:</p>
+  <p>👉 <em>Frederico Canoeira</em></p>
+  <p>👉 <em>Cleber Teixeira</em></p>
+  <p><strong>#GoTrybe!</strong> 🚀</p>`;
+  rightContent.appendChild(resume);
+  mainContent.appendChild(rightContent);
+}
+
+function confirmValidation() {
+  const firstName = document.querySelector('#first-name').value;
+  const lastName = document.querySelector('#last-name').value;
+  const phoneEmail = document.querySelector('#phone-email').value;
+  const newPassword = document.querySelector('#pass-word').value;
+  const birthDate = document.querySelector('#birthdate').value;
+  if (firstName !== '') {
+    if (lastName !== '') {
+      if (phoneEmail !== '') {
+        if (newPassword !== '') {
+          if (birthDate !== '') {
+            if (validationRadio() === true) {
+              clearForm();
+              confirmRegistration();
+            }
+          }
+        }
+      }
+    }
+  }
 }
 
 window.onload = function () {
