4b1490ddce96aad179d26cbe221944dd6f70d8b4
Marina Alane <marina.alane@outlook.com>
2021-01-16 17:13:20 -0300

    verifica campos vazios

--- script.js
+++ script.js
@@ -8,3 +8,15 @@ function clicar() {
 //  Interpretei errado e a mensagem do alert não era "email ou telefone" mas sim, o valor do//
 //  campo e-mail e telefone. Agora deu!//
 clicar();
+
+function verificaInputs(){
+  const inputs = document.querySelectorAll('input');
+   for (let i = 0; i < inputs.length; i += 1){
+     if(inputs[i] === '') {
+       const errorMsg = document.createElement('p');
+       errorMsg.innerText = 'Campos inválidos';
+     }
+   }
+}
+
+verificaInputs();
