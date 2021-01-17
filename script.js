a32136689fdda7038aa23035252b784afde39129
LeoMajevski <leonardo.majevski@gmail.com>
2021-01-17 19:55:18 -0300

    Fix: altera estrutura da mascara de datas (de jquery para vanilla js)

--- script.js
+++ script.js
@@ -90,8 +90,26 @@ function checkInputs(event) {
 }
 submitButton.addEventListener('click', checkInputs);
 
-// Date Mask in dd/mm/yyyy format
-$(document).ready(function () {
-  $('#input-birthdate').mask('99/99/9999');
-  return false;
-});
+// Masks for forms inputs
+function mask(i,t){
+  let v = i.value;
+  if(isNaN(v[v.length-1])){
+    i.value = v.substring(0, v.length-1);
+    return;
+  }
+  if(t == "date"){
+    i.setAttribute("maxlength", "10");
+    if (v.length == 2 || v.length == 5) i.value += "/";
+  }
+  if(t == "cpf"){
+    i.setAttribute("maxlength", "14");
+    if (v.length == 3 || v.length == 7) i.value += ".";
+    if (v.length == 11) i.value += "-";
+  }
+  if(t == "cnpj"){
+    i.setAttribute("maxlength", "18");
+    if (v.length == 2 || v.length == 6) i.value += ".";
+    if (v.length == 10) i.value += "/";
+    if (v.length == 15) i.value += "-";
+  }
+}
