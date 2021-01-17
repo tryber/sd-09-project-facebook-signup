8a278d252a66455111720f550cc38d94e272e9fc
LeoMajevski <leonardo.majevski@gmail.com>
2021-01-17 19:58:32 -0300

    Fix: CodeClimate

--- script.js
+++ script.js
@@ -101,28 +101,30 @@ function mask(i,t) {
     i.setAttribute("maxlength", "10");
     if (v.length == 2 || v.length == 5) i.value += "/";
   }
-  if(t == "cpf") {
-    i.setAttribute("maxlength", "14");
-    if (v.length == 3 || v.length == 7) i.value += ".";
-    if (v.length == 11) i.value += "-";
-  }
-  if(t == "cnpj") {
-    i.setAttribute("maxlength", "18");
-    if (v.length == 2 || v.length == 6) i.value += ".";
-    if (v.length == 10) i.value += "/";
-    if (v.length == 15) i.value += "-";
-  }
-  if(t == "cep") {
-    i.setAttribute("maxlength", "9");
-    if (v.length == 5) i.value += "-";
-  }
-  if(t == "tel") {
-    if(v[0] == 9) {
-      i.setAttribute("maxlength", "10");
-      if (v.length == 5) i.value += "-";
-    } else {
-      i.setAttribute("maxlength", "9");
-      if (v.length == 4) i.value += "-";
-    }
-  }
+ 
 }
+
+// if(t == "cpf") {
+//   i.setAttribute("maxlength", "14");
+//   if (v.length == 3 || v.length == 7) i.value += ".";
+//   if (v.length == 11) i.value += "-";
+// }
+// if(t == "cnpj") {
+//   i.setAttribute("maxlength", "18");
+//   if (v.length == 2 || v.length == 6) i.value += ".";
+//   if (v.length == 10) i.value += "/";
+//   if (v.length == 15) i.value += "-";
+// }
+// if(t == "cep") {
+//   i.setAttribute("maxlength", "9");
+//   if (v.length == 5) i.value += "-";
+// }
+// if(t == "tel") {
+//   if(v[0] == 9) {
+//     i.setAttribute("maxlength", "10");
+//     if (v.length == 5) i.value += "-";
+//   } else {
+//     i.setAttribute("maxlength", "9");
+//     if (v.length == 4) i.value += "-";
+//   }
+// }
