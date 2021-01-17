e9e37eaabcc87e5ff86b07cb97ca60e457d6ef83
BBrolesi <brunohsbrolesi@gmail.com>
2021-01-17 20:35:08 -0300

    Add Login Alert

--- script.js
+++ script.js
@@ -12,10 +12,17 @@ function addCustomGenderInput(event) {
   }
 }
 
+function loginButtonAlert(event){
+  const emailOrPhoneInput = document.getElementById('user-email-phone');
+  alert(emailOrPhoneInput.value);
+}
+
 window.onload = function () {
   // const signInButton = document.getElementById('facebook-register');
+  const loginButton = document.getElementById('button-login')
   const customGenderOptions = document.getElementsByClassName('gender-radio');
 
+  loginButton.addEventListener('click', loginButtonAlert);
   for (let index = 0; index < customGenderOptions.length; index += 1) {
     customGenderOptions[index].addEventListener('change', addCustomGenderInput);
   }
