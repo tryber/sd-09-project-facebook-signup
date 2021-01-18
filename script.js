1ada1dc4a9e8524834d0925f3fdf86ed305c82ca
Lucas Silva <martssilvapro@gmail.com>
2021-01-18 10:52:06 -0300

    ajustes codeclimate

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const buttonLogin = document.getElementById('button-login');
-//const buttonRegister = document.getElementById('facebook-register');
+//  const buttonRegister = document.getElementById('facebook-register');
 const genderCustom = document.getElementById('other');
 const formGroup = document.querySelector('.right-content .form-group');
 
@@ -8,33 +8,32 @@ buttonLogin.addEventListener('click', function () {
   window.alert(userEmail.value);
 });
 
-//Validar form de cadastro
+//  Validar form de cadastro
 
 function validateForm() {
-
   const formInputs = document.querySelectorAll('#register-form input');
   let hasInputEmpty = false;
   for (index = 0; index < formInputs.length; index += 1) {
     const inputs = formInputs[index].value;
     if (inputs === '') {
-      hasInputEmpty = true;   
+      hasInputEmpty = true;
     }
   }
-    
-  //Verifica se tem input vazio e exibe mensagem de erro
+
+  //  Verifica se tem input vazio e exibe mensagem de erro
   if (hasInputEmpty) {
-    let form = document.querySelector('#register-form');
-    let errorMessage = document.createElement('p');
+    const form = document.querySelector('#register-form');
+    const errorMessage = document.createElement('p');
     errorMessage.textContent = 'Campos inválidos';
     errorMessage.id = 'form-message';
     form.appendChild(errorMessage);
   }else{
-    //Aqui eu vou subtituir o html pelo valor do input
+    //  Aqui eu vou subtituir o html pelo valor do input
     const formCompleted = document.querySelector('.right-content');
     formCompleted.innerHTML = '';
     for (index = 0; index < formInputs.length; index += 1) {
       const element = document.createElement('p');
-      let formContent = formInputs[index].value;
+      const formContent = formInputs[index].value;
       element.innerHTML = formContent;
       formCompleted.appendChild(element);
     }
@@ -42,8 +41,8 @@ function validateForm() {
 }
 
 function btn() {
-  let button = document.querySelector('#facebook-register')
-  button.addEventListener('click', validateForm)
+  const button = document.querySelector('#facebook-register');
+  button.addEventListener('click', validateForm);
 }
 
 btn();
@@ -54,7 +53,7 @@ function createInputText() {
   createInput.name = 'gender-custom';
   createInput.placeholder = 'Gênero (opcional)';
   return createInput;
-};
+}
 genderCustom.addEventListener('click', function () {
   const register = document.querySelector('.form-group .gender-custom');
   if (register === null) {
