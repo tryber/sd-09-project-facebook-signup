d500b4148c9d06be99c618d4efcf0ae62a835867
Diego Vini <analistamm@gmail.com>
2021-01-18 11:06:49 -0300

    Adicionando comentarios no scripts.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 document.querySelector('#button-login').addEventListener('click', function () {
   alert(document.getElementById('user-email-phone').value);
 });
+
+// function validate () {
+//   var firstname = form-register.firstname.value;
+//   var lastname = form-register.lastname.value;
+//   var phone_email = form-register.phone_email.value;
+// }
