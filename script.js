d78881dae5101b9888ccf48e94190ec23bb1866c
otacisos <otacisods@gmail.com>
2021-01-18 12:29:35 -0300

    Ultimo commit do projeto em grupo fix9

--- script.js
+++ script.js
@@ -7,6 +7,18 @@ function falert(event) {
 }
 button.addEventListener('click', falert);
 
+const rdoPerso = document.getElementById('personalizado');
+function inputTxtPerso() {
+  const rdoPerso2 = document.querySelector('.radios');
+  const rdoPersoNew = document.createElement('input');
+  const rdoPersoNewName = 'gender-custom';
+  const rdoPersoNewPh = 'Gênero (opcional)';
+  rdoPersoNew.name = rdoPersoNewName;
+  rdoPersoNew.placeholder = rdoPersoNewPh;
+  rdoPerso2.appendChild(rdoPersoNew);
+}
+rdoPerso.addEventListener('click', inputTxtPerso);
+
 function createP() {
   const newP = document.querySelector('.form');
   const newP2 = document.createElement('p');
@@ -15,51 +27,18 @@ function createP() {
   newP.appendChild(newP2);
 }
 
-const btnCadastre = document.getElementById('facebook-register');
-function validInput(event) {
-  event.preventDefault();
-  const inputName = document.getElementById('name');
-  const inputLastname = document.getElementById('lastname');
-  const inputPhone = document.getElementById('phoneemail');
-  const inputNewpw = document.getElementById('newpassword');
-  const inputBirth = document.getElementById('birthdate');
-  const inputFeminino = document.getElementById('feminino');
-  const inputMasculino = document.getElementById('masculino');
-  const inputPersonalizado = document.getElementById('personalizado');
-  const inputNameValue = inputName.value;
-  const inputLastnameValue = inputLastname.value;
-  const inputPhoneValue = inputPhone.value;
-  const inputNewpwValue = inputNewpw.value;
-  const inputBirthValue = inputBirth.value;
-  const inputFemininoValue = inputFeminino.checked;
-  const inputMasculinoValue = inputMasculino.checked;
-  const inputPersonalizadoValue = inputPersonalizado.checked;
-  if (inputNameValue === '' || inputLastnameValue === '' || inputPhoneValue === '' || inputNewpwValue === '' || inputBirthValue === '') {
-    createP();
-  } else if (inputFemininoValue === false && inputMasculinoValue === false && inputPersonalizadoValue === false) {
-    createP();
-  } else {
-    write();
-  }
-}
-btnCadastre.addEventListener('click', validInput);
-
 function write() {
   const box = document.getElementById('right-content');
-  const inputName2 = document.getElementById('name');
-  const inputLastname2 = document.getElementById('lastname');
-  const inputPhone2 = document.getElementById('phoneemail');
-  const inputBirth2 = document.getElementById('birthdate');
   const inputFeminino2 = document.getElementById('feminino');
   const inputMasculino2 = document.getElementById('masculino');
   const inputPersonalizado2 = document.getElementById('personalizado');
-  const inputNameValue2 = inputName2.value;
-  const inputLastnameValue2 = inputLastname2.value;
-  const inputPhoneValue2 = inputPhone2.value;
-  const inputBirthValue2 = inputBirth2.value;
-  const inputFemininoValue2 = inputFeminino2.checked;
-  const inputMasculinoValue2 = inputMasculino2.checked;
-  const inputPersonalizadoValue2 = inputPersonalizado2.checked;
+  const inputs = document.querySelectorAll('.input');
+
+  const array = [];
+  for (let index = 0; index < inputs.length; index += 1) {
+    array.push(inputs[index].value);
+  }
+
   let checked2 = '';
   if (inputFeminino2.checked === true) {
     checked2 = 'Feminino';
