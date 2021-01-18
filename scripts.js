4b3b7ee0247b0166333ac98993a22d4f8d40822e
Mathaus <mathausannibal@gmail.com>
2021-01-18 11:31:48 -0300

    ultimo commit

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 window.onload = function () {
   const button = document.querySelector('#button-login');
-
+//mudança de commit para submissão do projeto
   const input = document.querySelector('#user-email-phone');
 
   button.addEventListener('click', function () {
