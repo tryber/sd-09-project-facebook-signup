a8301ec827be5585bf5d175434a618ef721fbbc9
Enio Nicael <enionicael@gmail.com>
2021-01-18 09:35:22 -0300

    Refatoracao da funcao handleValidateFields.

--- script.js
+++ script.js
@@ -9,56 +9,77 @@ buttonLogin.addEventListener('click', handleClickButtonLogin);
 
 const formSubmit = document.querySelector('.facebook-register');
 
-function customFieldIsShow() {
-  const customFieldElement = document.querySelector('.gender-custom');
-  const customFieldDisplay = customFieldElement.style.display;
-  if (customFieldDisplay === 'block') {
-    if (customFieldElement.value === '') {
+const validateInputsText = () => {
+  const inputsText = document.querySelectorAll('.facebook-register input[type="text"');
+  const customIsChecked = document.querySelector('.custom-radio').checked;
+  let inputsLength;
+  if (!customIsChecked) {
+    inputsLength = inputsText.length - 1;
+  } else {
+    inputsLength = inputsText.length;
+  }
+  for (let index = 0; index < inputsLength; index += 1) {
+    if (inputsText[index].value.length === 0) {
       return false;
     }
   }
   return true;
-}
+};
 
-function handleValidateFields(event) {
-  event.preventDefault();
-  let filledFields = true;
+const validateInputsPassword = () => {
+  const inputPassword = document.querySelector('.facebook-register input[type="password"');
+  if (inputPassword.value.length === 0) {
+    return false;
+  }
+  return true;
+};
 
-  const customField = customFieldIsShow();
+const validateFields = () => {
+  const inputsTextOk = validateInputsText();
+  const inputsPasswordOk = validateInputsPassword();
+  const alertMessage = document.getElementById('alert');
+  if (!inputsTextOk || !inputsPasswordOk) {
+    alertMessage.style.display = 'block';
+    return false;
+  } else {
+    alertMessage.style.display = 'none';
+    return true;
+  }
+};
 
+const getRegisterResult = () => {
   const inputs = document.querySelectorAll('.facebook-register input');
   for (let index = 0; index < inputs.length; index += 1) {
-    if (inputs[index].value === '' && customField === false) {
-      const alertMessage = document.getElementById('alert');
-      alertMessage.style.display = 'block';
-      filledFields = false;
-      return;
+    if (inputs[index].name === 'firstname') {
+      const greeting = document.querySelector('.greeting');
+      greeting.innerText = `Olá, ${inputs[index].value} ${inputs[index + 1].value}`;
+    } else if (inputs[index].name === 'phone_email') {
+      const phoneMail = document.querySelector('.email-phone');
+      phoneMail.innerText = inputs[index].value;
+    } else if (inputs[index].name === 'birthdate') {
+      const datOfBirth = document.querySelector('.date-of-birth');
+      datOfBirth.innerText = inputs[index].value;
+    } else if (inputs[index].name === 'gender' && inputs[index].checked === true) {
+      const genderResult = document.querySelector('.gender-result');
+      genderResult.innerText = inputs[index].value;
     }
   }
+};
 
-  if (filledFields) {
-    for (let index = 0; index < inputs.length; index += 1) {
-      if (inputs[index].name === 'firstname') {
-        const greeting = document.querySelector('.greeting');
-        greeting.innerText = `Olá, ${inputs[index].value} ${inputs[index + 1].value}`;
-      } else if (inputs[index].name === 'phone_email') {
-        const phoneMail = document.querySelector('.email-phone');
-        phoneMail.innerText = inputs[index].value;
-      } else if (inputs[index].name === 'birthdate') {
-        const datOfBirth = document.querySelector('.date-of-birth');
-        datOfBirth.innerText = inputs[index].value;
-      } else if (inputs[index].name === 'gender' && inputs[index].checked === true) {
-        const genderResult = document.querySelector('.gender-result');
-        genderResult.innerText = inputs[index].value;
-      }
-    }
-
+const displayRegisterResult = (isCompleted) => {
+  if (isCompleted) {
+    getRegisterResult();
     const register = document.querySelector('.register');
     register.style.display = 'none';
-
     const result = document.querySelector('.result');
     result.style.display = 'block';
   }
+};
+
+function handleValidateFields(event) {
+  event.preventDefault();
+  const filledFields = validateFields();
+  displayRegisterResult(filledFields);
   // formSubmit.submit();
 }
 
