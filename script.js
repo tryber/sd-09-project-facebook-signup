edd63bc844114147acd51f38f40cc795d3bef441
Arlesson Moura <arlessonmss@gmail.com>
2021-01-18 13:04:00 -0300

    80%

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 const button = document.querySelector('#button-login');
-console.log(button);
-
 button.addEventListener('click', function () {
     const inputUser = document.querySelector('#user-email-phone').value;
   alert(inputUser);
 });
-
-
