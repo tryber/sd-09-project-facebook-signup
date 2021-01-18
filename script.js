f8eca99103f5f0dbdc68d30809e0af418d5ff8a4
LeoMajevski <leonardo.majevski@gmail.com>
2021-01-17 23:22:05 -0300

    Refatoring: function mask()

--- script.js
+++ script.js
@@ -91,41 +91,13 @@ function checkInputs(event) {
 submitButton.addEventListener('click', checkInputs);
 
 // Masks for forms inputs
-function mask(i, t) {
+function mask(i) {
   const v = i.value;
   if (isNaN(v[v.length - 1])) {
     i.value = v.substring(0, v.length - 1);
     return;
   }
-  if (t === 'date') {
     i.setAttribute('maxlength', '10');
     if (v.length === 2 || v.length === 5) i.value += '/';
-  }
 }
-
-mask();
-
-// if(t == "cpf") {
-//   i.setAttribute("maxlength", "14");
-//   if (v.length == 3 || v.length == 7) i.value += ".";
-//   if (v.length == 11) i.value += "-";
-// }
-// if(t == "cnpj") {
-//   i.setAttribute("maxlength", "18");
-//   if (v.length == 2 || v.length == 6) i.value += ".";
-//   if (v.length == 10) i.value += "/";
-//   if (v.length == 15) i.value += "-";
-// }
-// if(t == "cep") {
-//   i.setAttribute("maxlength", "9");
-//   if (v.length == 5) i.value += "-";
-// }
-// if(t == "tel") {
-//   if(v[0] == 9) {
-//     i.setAttribute("maxlength", "10");
-//     if (v.length == 5) i.value += "-";
-//   } else {
-//     i.setAttribute("maxlength", "9");
-//     if (v.length == 4) i.value += "-";
-//   }
-// }
+document.querySelector('#input-birthdate').oninput = function() {mask(this)};
