de70e151bd7fc84eb51eca84a95914f89ed0fe98
Diego Vini <analistamm@gmail.com>
2021-01-18 11:15:44 -0300

    Ajustando cc

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
 document.querySelector('#button-login').addEventListener('click', function () {
   alert(document.getElementById('user-email-phone').value);
 });
-//Requisito 18
-// function validate () {
-//   var firstname = form-register.firstname.value;
-//   var lastname = form-register.lastname.value;
-//   var phone_email = form-register.phone_email.value;
-// }
